d5f1eec5f2cf76b8ee2829159306126710938d9d
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2016-06-14 22:13:35 +1000

    fixed order of PATH and PYTHONPATH

--- pbs/submit_ipyparallel.sh
+++ pbs/submit_ipyparallel.sh
@@ -6,8 +6,8 @@
 
 # setup environment
 module load zlib atlas python3/3.4.3 hdf5/1.8.10 gdal/2.0.0 zeromq/4.1.3
-export PATH=$PATH:$HOME/.local/bin
-export PYTHONPATH=$PYTHONPATH:$HOME/.local/lib/python3.4/site-packages
+export PATH=$HOME/.local/bin:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.4/site-packages:$PYTHONPATH
 export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.4.3/bin/python3                 
 source $HOME/.local/bin/virtualenvwrapper.sh 
 
