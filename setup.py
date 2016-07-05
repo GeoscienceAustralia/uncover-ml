e6976df1548f1df97deb127b21ca313ab9d0b553
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2016-07-05 10:48:57 +1000

    make demo scripts actual scripts

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
+            'learningpipeline = uncoverml.scripts.learningpipeline:main',
+            'predictionpipeline = uncoverml.scripts.predictionpipeline:main',
             'maketargets = uncoverml.scripts.maketargets:main',
             'cvindexer = uncoverml.scripts.cvindexer:main',
             'extractfeats = uncoverml.scripts.extractfeats:main',
