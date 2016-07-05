d5fed54decb417d112d3c8db7a83bdd3a04a0078
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2016-07-05 13:42:35 +1000

    refactored scripts to use config file

--- uncoverml/scripts/learningpipeline.py
+++ uncoverml/scripts/learningpipeline.py
@@ -1,9 +1,9 @@
-#! /usr/bin/env python
 """
 A demo script that ties some of the command line utilities together in a
 pipeline for learning and validating models.
 """
 
+import importlib.machinery
 import logging
 import json
 from os import path, mkdir, listdir
@@ -24,144 +24,21 @@ from uncoverml.scripts.validatemodel import main as validatemodel
 log = logging.getLogger(__name__)
 
 
-# NOTE: INSTRUCTIONS ----------------------------------------------------------
-#   1) Make sure you have open MPI install (i.e. mpirun)
-#   2) Make sure you have all of the data in the directory structure specified
-#      below (or change it to suit your purposes)
-#   3) run this script using mpirun with n + 2 workers, e.g.
-#       $ mpirun -n 6 demo_learning_pipline.py
-#      for 4 workers (we need two workers for coordination)
-# -----------------------------------------------------------------------------
-
-
-#
-# Path Settings
-#
-
-# As this is set now, it assumes a folder structure like the following:
-# ~/data/GA-cover   << location of data (all tif and shp files)
-# ~/data/GA-cover/processed     << location of all output files
-# ~/data/GA-cover/soilcrossvalindices.hdf5   << cross val hdf5 file from us
-
-# please change the following paths to suit your needs
-
-# Location of data
-data_dir = path.join(path.expanduser("~"), "data/GA-cover")
-# data_dir = path.join(path.expanduser("~"), "data/GA-depth")
-# data_dir = "/short/ge3/jrw547/Murray_datasets"
-# data_dir = "/short/ge3/jrw547/GA-cover"
-
-# Location of processed files (features, predictions etc)
-proc_dir = path.join(data_dir, "processed")
-# proc_dir = "/short/ge3/dms599/Murray_processed"
-# proc_dir = "/short/ge3/dms599/GA-cover_processed"
-
-
-#
-# Target Settings
-#
-
-# Shape file with target variable info
-target_file = "geochem_sites.shp"
-# target_file = "drillhole_confid_3.shp"
-# target_file = "Targets_V8.shp"
-
-# Target variable name (in shape file)
-target_var = "Na_ppm_i_1"  # "Cr_ppm_i_1"
-# target_var = "depth"
-
-# Where to save processed targets
-target_hdf = path.join(proc_dir, "{}_{}.hdf5"
-                       .format(path.splitext(target_file)[0], target_var))
-
-folds = 5
-
-
-#
-# Feature settings
-#
-
-# Allows deternministic cross-val. Set to None for a random seed
-crossval_seed = 1
-
-# Automatically detect integer-valued files and use one-hot encoding?
-onehot = False
-
-# Patch size to extract around targets (0 = 1x1 pixel, 1 = 3x3 pixels etc)
-patchsize = 0
-
-# Impute missing values?
-impute = True
-
-# Starndardise each input dimension? (0 mean, 1 std)
-standardise = True  # standardise all of the extracted features?
-
-# Whiten all inputs?
-whiten = False  # whiten all of the extracted features?
-
-# Fraction of dimensions to keep *if* whitening
-pca_frac = 0.5
-
-# Composite feature names (prefixes)
-compos_file = "composite"
-
-
-#
-# Algorithm settings
-#
-
-# Iterate through this dictionary of algorithm name and arguments:
-algdict = {
-
-    # Bayesian linear regression
-    # "bayesreg": {},
-
-    # Approximate Gaussian process, for large scale data
-    # "approxgp": {'kern': 'matern52', 'lenscale': [100.] * 87, 'nbases': 50},
-    # "approxgp": {'kern': 'rbf', 'lenscale': 100., 'nbases': 50},
-
-    # Support vector machine (regressor)
-    # "svr": {'gamma': 1. / 300, 'epsilon': 0.05},
-    "svr": {},
-
-    # Random forest regressor
-    # "randomforest": {'n_estimators': 100},
-
-    # ARD Linear regression
-    # "ardregression": {},
-
-    # Kernel ridge regression
-    # 'kernelridge': {'kernel': 'rbf'},
-
-    # Decision tree regressor
-    # 'decisiontree': {},
-
-    # Extra tree regressor
-    # 'extratree': {},
-}
-
-# Prediction file names (prefix)
-predict_file = "prediction_file"
-
-# Output suffix files for validation metrics
-valoutput = "validation"
-
-
 # NOTE: Do not change the following unless you know what you are doing
-def run_pipeline():
+def run_pipeline(config):
 
     # MPI globals
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
     # Make processed dir if it does not exist
-    if not path.exists(proc_dir) and rank == 0:
-        mkdir(proc_dir)
+    if not path.exists(config.proc_dir) and rank == 0:
+        mkdir(config.proc_dir)
         log.info("Made processed dir")
     comm.barrier()
 
     # Make sure the directory is empty if it does exist
-    if listdir(proc_dir):
+    if listdir(config.proc_dir):
         log.fatal("Output directory must be empty!")
         sys.exit(-1)
 
@@ -169,18 +46,18 @@ def run_pipeline():
     if rank == 0:
         ctx = Context(maketargets)
         ctx.forward(maketargets,
-                    shapefile=path.join(data_dir, target_file),
-                    fieldname=target_var,
-                    folds=folds,
-                    outfile=target_hdf,
-                    seed=crossval_seed
+                    shapefile=path.join(config.data_dir, config.target_file),
+                    fieldname=config.target_var,
+                    folds=config.folds,
+                    outfile=config.target_hdf,
+                    seed=config.crossval_seed
                     )
     comm.barrier()
 
     # Extract feats for training
-    tifs = glob(path.join(data_dir, "*.tif"))
+    tifs = glob(path.join(config.data_dir, "*.tif"))
     if len(tifs) == 0:
-        log.fatal("No geotiffs found in {}!".format(data_dir))
+        log.fatal("No geotiffs found in {}!".format(config.data_dir))
         sys.exit(-1)
 
     # Generic extract feats command
@@ -193,72 +70,75 @@ def run_pipeline():
         ctx.forward(extractfeats,
                     geotiff=tif,
                     name=name,
-                    outputdir=proc_dir,
-                    targets=target_hdf,
-                    onehot=onehot,
-                    patchsize=patchsize
+                    outputdir=config.proc_dir,
+                    targets=config.target_hdf,
+                    onehot=config.onehot,
+                    patchsize=config.patchsize
                     )
         comm.barrier()
 
-    efiles = [f for f in glob(path.join(proc_dir, "*.part*.hdf5"))
-              if not (path.basename(f).startswith(compos_file)
-                      or path.basename(f).startswith(predict_file))]
+    efiles = [f for f in glob(path.join(config.proc_dir, "*.part*.hdf5"))
+              if not (path.basename(f).startswith(config.compos_file)
+                      or path.basename(f).startswith(config.predict_file))]
 
     # Compose individual image features into single feature vector
     log.info("Composing features...")
     ctx = Context(composefeats)
     ctx.forward(composefeats,
-                featurename=compos_file,
-                outputdir=proc_dir,
-                impute=impute,
-                centre=standardise or whiten,
-                standardise=standardise,
-                whiten=whiten,
-                featurefraction=pca_frac,
+                featurename=config.compos_file,
+                outputdir=config.proc_dir,
+                impute=config.impute,
+                centre=config.standardise or config.whiten,
+                standardise=config.standardise,
+                whiten=config.whiten,
+                featurefraction=config.pca_frac,
                 files=efiles
                 )
     comm.barrier()
 
-    feat_files = glob(path.join(proc_dir, compos_file + ".part*.hdf5"))
+    feat_files = glob(path.join(config.proc_dir,
+                                config.compos_file + ".part*.hdf5"))
 
-    for alg, args in algdict.items():
+    for alg, args in config.algdict.items():
 
         # Train the model
         log.info("Training model {}.".format(alg))
         ctx = Context(learnmodel)
         ctx.forward(learnmodel,
-                    outputdir=proc_dir,
+                    outputdir=config.proc_dir,
                     cvindex=0,
                     algorithm=alg,
                     algopts=json.dumps(args),
-                    targets=target_hdf,
+                    targets=config.target_hdf,
                     files=feat_files
                     )
         comm.barrier()
 
         # Test the model
         log.info("Predicting targets for {}.".format(alg))
-        alg_file = path.join(proc_dir, "{}.pk".format(alg))
+        alg_file = path.join(config.proc_dir, "{}.pk".format(alg))
 
         ctx = Context(predict)
         ctx.forward(predict,
-                    outputdir=proc_dir,
-                    predictname=predict_file + "_" + alg,
+                    outputdir=config.proc_dir,
+                    predictname=config.predict_file + "_" + alg,
                     model=alg_file,
                     files=feat_files
                     )
         comm.barrier()
 
-        pred_files = glob(path.join(proc_dir, predict_file + "_" + alg +
+        pred_files = glob(path.join(config.proc_dir,
+                                    config.predict_file + "_" + alg +
                                     ".part*.hdf5"))
 
         # Report score
         log.info("Validating {}.".format(alg))
         ctx = Context(validatemodel)
         ctx.forward(validatemodel,
-                    outfile=path.join(proc_dir, valoutput + "_" + alg),
+                    outfile=path.join(config.proc_dir,
+                                      config.valoutput + "_" + alg),
                     cvindex=0,
-                    targets=target_hdf,
+                    targets=config.target_hdf,
                     prediction_files=pred_files,
                     plotyy=False
                     )
@@ -268,9 +148,14 @@ def run_pipeline():
 
 
 def main():
-
+    if (sys.argv) != 2:
+        print("Usage: learningpipeline <configfile>")
+        sys.exit(-1)
     logging.basicConfig(level=logging.INFO)
-    run_pipeline()
+    config_filename = sys.argv[1]
+    config = importlib.machinery.SourceFileLoader(
+        'config', config_filename).load_module()
+    run_pipeline(config)
 
 if __name__ == "__main__":
     main()
--- uncoverml/scripts/predictionpipeline.py
+++ uncoverml/scripts/predictionpipeline.py
@@ -4,6 +4,7 @@ A demo script that ties some of the command line utilities together in a
 pipeline for prediction.
 """
 
+import importlib.machinery
 import sys
 import logging
 from os import path, mkdir, listdir
@@ -17,112 +18,40 @@ from uncoverml.scripts.composefeats import main as composefeats
 from uncoverml.scripts.predict import main as predict
 from uncoverml.scripts.exportgeotiff import main as exportgeotiff
 
-from runcommands import PipeLineFailure
-
-
 log = logging.getLogger(__name__)
 
 
-# NOTE: INSTRUCTIONS ----------------------------------------------------------
-#   1) Make sure you have open MPI install (i.e. mpirun)
-#   2) Make sure you have all of the data in the directory structure specified
-#      below (or change it to suit your purposes)
-#   3) Make sure you have a learned model by running demo_learning_pipeline.py
-#   4) run this script using mpirun with n + 2 workers, e.g.
-#       $ mpirun -n 6 demo_prediction_pipline.py
-#      for 4 workers (we need two workers for coordination)
-# -----------------------------------------------------------------------------
-
-#
-# Path Settings
-#
-
-# As this is set now, it assumes a folder structure like the following:
-# ~/data/GA-cover   << location of data (all tif and shp files)
-# ~/data/GA-cover/processed     << location of output from learning pipeline
-# ~/data/GA-cover/processed/prediction     << location of prediction output
-
-# please change the following paths to suit your needs
-
-# Location of data
-data_dir_name = "data/GA-cover"
-# data_dir_name = "data/GA-depth"
-data_dir = path.join(path.expanduser("~"), data_dir_name)
-# data_dir = "/short/ge3/jrw547/Murray_datasets"
-# data_dir = "/short/ge3/jrw547/GA-cover"
-
-# Location of processed file (features, predictions etc)
-# proc_dir = "/short/ge3/dms599/Murray_processed"
-# proc_dir = "/short/ge3/dms599/GA-cover_processed"
-proc_dir = path.join(data_dir, "processed")
-
-# Location of the prediction output from this script
-pred_dir = path.join(proc_dir, "prediction")
-
-# Composite feature names (prefixes)
-compos_file = "composite"
-
-
-#
-# Prediction settings
-#
-
-# Name of the prediction algorithm
-algorithm = 'svr'
-# algorithm = 'bayesreg'
-# algorithm = 'approxgp'
-# algorithm = 'randomforest'
-
-# Prediction file names (prefix)
-predict_file = "prediction"
-
-# Quantiles
-quantiles = 0.95
-
-
-#
-# Visualisation/Geotiff settings
-#
-
-# Name of the prediction output tif
-gtiffname = "prediction_image"
-gtiffname_ent = "entropy_reduction_image"
-
-# Make the image RGB?
-makergbtif = True
-
-
-# NOTE: Do not change the following unless you know what you are doing
-def run_pipeline():
+def run_pipeline(config):
 
     # MPI globals
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    if not path.exists(proc_dir):
+    if not path.exists(config.proc_dir):
         log.fatal("Please run demo_learning_pipline.py first!")
         sys.exit(-1)
 
     # Make processed dir if it does not exist
-    if not path.exists(pred_dir) and rank == 0:
-        mkdir(pred_dir)
+    if not path.exists(config.pred_dir) and rank == 0:
+        mkdir(config.pred_dir)
         log.info("Made prediction dir")
     comm.barrier()
 
     # Make sure prediction dir is empty
-    if listdir(pred_dir):
+    if listdir(config.pred_dir):
         log.fatal("Prediction directory must be empty!")
         sys.exit(-1)
 
     # Make sure we have an extractfeats settings file for each tif
-    tifs = glob(path.join(data_dir, "*.tif"))
+    tifs = glob(path.join(config.data_dir, "*.tif"))
     if len(tifs) == 0:
-        log.fatal("No geotiffs found in {}!".format(data_dir))
+        log.fatal("No geotiffs found in {}!".format(config.data_dir))
         sys.exit(-1)
 
     settings = []
     for tif in tifs:
-        setting = path.join(proc_dir, path.splitext(path.basename(tif))[0] +
+        setting = path.join(config.proc_dir, 
+                            path.splitext(path.basename(tif))[0] +
                             "_settings.bin")
         if not path.exists(setting):
             log.fatal("Setting file {} does not exist!".format(setting))
@@ -139,62 +68,63 @@ def run_pipeline():
         ctx.forward(extractfeats,
                     geotiff=tif,
                     name=name,
-                    outputdir=pred_dir,
+                    outputdir=config.pred_dir,
                     settings=setting
                     )
         comm.barrier()
 
     # Compose individual image features into single feature vector
-    compos_settings = path.join(proc_dir, compos_file + "_settings.bin")
+    compos_settings = path.join(config.proc_dir, 
+                                config.compos_file + "_settings.bin")
     if not path.exists(compos_settings):
         log.fatal("Settings file for composite features does not exist!")
         sys.exit(-1)
 
-    efiles = [f for f in glob(path.join(pred_dir, "*.part*.hdf5"))
-              if not (path.basename(f).startswith(compos_file) or
-                      path.basename(f).startswith(predict_file))]
+    efiles = [f for f in glob(path.join(config.pred_dir, "*.part*.hdf5"))
+              if not (path.basename(f).startswith(config.compos_file) or
+                      path.basename(f).startswith(config.predict_file))]
 
     log.info("Composing features...")
     ctx = Context(composefeats)
     ctx.forward(composefeats,
-                featurename=compos_file,
-                outputdir=pred_dir,
+                featurename=config.compos_file,
+                outputdir=config.pred_dir,
                 files=efiles,
                 settings=compos_settings
                 )
     comm.barrier()
 
-    cfiles = glob(path.join(pred_dir, compos_file + "*.hdf5"))
+    cfiles = glob(path.join(config.pred_dir, config.compos_file + "*.hdf5"))
 
     # Now predict on the composite features!
-    alg_file = path.join(proc_dir, "{}.pk".format(algorithm))
+    alg_file = path.join(config.proc_dir, "{}.pk".format(config.algorithm))
     if not path.exists(alg_file):
         log.fatal("Learned algorithm file {} missing!".format(alg_file))
         sys.exit(-1)
 
     alg = path.splitext(path.basename(alg_file))[0]
-    predict_alg_file = predict_file + '_' + alg
+    predict_alg_file = config.predict_file + '_' + alg
 
     log.info("Predicting targets...")
     ctx = Context(predict)
     ctx.forward(predict,
-                outputdir=pred_dir,
+                outputdir=config.pred_dir,
                 predictname=predict_alg_file,
                 model=alg_file,
-                quantiles=quantiles,
+                quantiles=config.quantiles,
                 files=cfiles
                 )
     comm.barrier()
 
-    pfiles = glob(path.join(pred_dir, predict_alg_file + '*.hdf5'))
+    pfiles = glob(path.join(config.pred_dir, predict_alg_file + '*.hdf5'))
 
     # Output a Geotiff of the predictions
     log.info("Exporting geoftiffs...")
     ctx = Context(exportgeotiff)
     ctx.forward(exportgeotiff,
-                name=gtiffname + "_" + alg,
-                outputdir=pred_dir,
-                rgb=makergbtif,
+                name=config.gtiffname + "_" + alg,
+                outputdir=config.pred_dir,
+                rgb=config.makergbtif,
                 files=pfiles
                 )
     comm.barrier()
@@ -203,9 +133,14 @@ def run_pipeline():
 
 
 def main():
-
+    if (sys.argv) != 2:
+        print("Usage: predictionpipeline <configfile>")
+        sys.exit(-1)
     logging.basicConfig(level=logging.INFO)
-    run_pipeline()
+    config_filename = sys.argv[1]
+    config = importlib.machinery.SourceFileLoader(
+        'config', config_filename).load_module()
+    run_pipeline(config)
 
 if __name__ == "__main__":
     main()
