510726c0ceacafe6213075a41d4730622f743dc5
Daniel Steinberg <dan.m.steinberg@gmail.com>
2016-07-13 16:30:19 +1000

    likelihood has been changed and works

--- uncoverml/likelihoods.py
+++ uncoverml/likelihoods.py
@@ -1,37 +1,27 @@
 from __future__ import division
 
 import numpy as np
-from scipy.special import erf, expit
-from scipy.stats import norm
+from scipy.special import erf
 
 from revrand.likelihoods import Bernoulli, Gaussian
 from revrand.btypes import Parameter, Positive
-from revrand.mathfun.special import softplus
 
 
 # Constants
-SQRTPI2 = np.sqrt(np.pi) / 2
+SQRTPI2 = np.sqrt(np.pi / 2)
 
 
 class Switching(Bernoulli):
 
-    def __init__(self, var_init=Parameter(1., Positive())):
+    def __init__(self, lenscale=1., var_init=Parameter(1., Positive())):
 
         self.params = var_init
         self.gaus = Gaussian(var_init)
-        self.unif = UnifGauss()
-
-        # FIXME: TEMP
-        self.i = 0
+        self.unif = UnifGauss(lenscale)
 
     def loglike(self, y, f, var, z):
         loglike = self.__split_on_z(self.unif.loglike, self.gaus.loglike, y, f,
                                     var, z)
-        # FIXME: TEMP
-        if (self.i % 1000) == 0:
-            print("fmin: {}, fmax: {}, fshape: {}".format(f.min(), f.max(),
-                                                          f.shape))
-        self.i += 1
         return loglike
 
     def Ey(self, f, var, z):
@@ -80,183 +70,176 @@ class Switching(Bernoulli):
         return val
 
 
-class PosGaussian(Gaussian):
-
-    def loglike(self, y, f, var):
-
-        return norm.logpdf(y, loc=softplus(f), scale=np.sqrt(var))
-
-    def Ey(self, y, f, var):
-
-        return softplus(f)
-
-    def df(self, y, f, var):
-
-        g = softplus(f)
-        gp = expit(f)
-
-        df = (y - g) * gp / var
-        return df
-
-    def d2f(self, y, f, var):
-
-        g = softplus(f)
-        gp = expit(f)
-        g2p = gp * (1 - gp)
-
-        d2f = ((y - g) * g2p - gp**2) / var
-        return d2f
-
-    def d3f(self, y, f, var):
-
-        g = softplus(f)
-        gp = expit(f)
-        g2p = gp * (1 - gp)
-        g3p = g2p * (1 - 2 * gp)
-
-        d3f = ((y - g) * g3p - 3 * gp * g2p) / var
-        return d3f
-
-    def dp(self, y, f, var):
-
-        g = softplus(f)
-        ivar = 1. / var
-        return 0.5 * (((y - g) * ivar)**2 - ivar)
-
-    def dpd2f(self, y, f, var):
-
-        dpd2f = - self.d2f(y, f, var) / var
-        return dpd2f
-
-    def cdf(self, y, f, var):
-
-        return norm.cdf(y, loc=softplus(f), scale=np.sqrt(var))
-
-
 class UnifGauss(Bernoulli):
+
+    def __init__(self, lenscale=1.):
+
+        self.l = lenscale
 
     def loglike(self, y, f):
 
-        return self.__split_apply(self._loglike_unif, self._loglike_gaus, y, f)
+        ll = self.__split_apply(self._loglike_y_lt_f,
+                                self._loglike_y_gt_f,
+                                self._loglike_f_lt_0, y, f)
 
-    def _loglike_unif(self, y, f, g):
+        return ll
 
-        return - np.log(g + SQRTPI2)
+    def _loglike_y_lt_f(self, y, f):
 
-    def _loglike_gaus(self, y, f, g):
+        return - np.log(f + self.l * SQRTPI2)
 
-        return - np.log(g + SQRTPI2) - (y - g)**2
+    def _loglike_y_gt_f(self, y, f):
+
+        return self._loglike_y_lt_f(y, f) - 0.5 * ((y - f) / self.l)**2
+
+    def _loglike_f_lt_0(self, y, f):
+
+        return - np.log(self.l * SQRTPI2) - 0.5 * ((y - f) / self.l)**2
 
     def pdf(self, y, f):
 
-        return self.__split_apply(self._pdf_unif, self._pdf_gaus, y, f)
+        p = self.__split_apply(self._pdf_y_lt_f,
+                               self._pdf_y_gt_f,
+                               self._pdf_f_lt_0,
+                               y, f)
 
-    def _pdf_unif(self, y, f, g):
+        return p
 
-        return 1 / (g + SQRTPI2)
+    def _pdf_y_lt_f(self, y, f):
 
-    def _pdf_gaus(self, y, f, g):
+        return 1 / (f + self.l * SQRTPI2)
 
-        return np.exp(-(y - g)**2) / (g + SQRTPI2)
+    def _pdf_y_gt_f(self, y, f):
+
+        return np.exp(-0.5 * ((y - f) / self.l)**2) * self._pdf_y_lt_f(y, f)
+
+    def _pdf_f_lt_0(self, y, f):
+
+        return np.exp(-0.5 * ((y - f) / self.l)**2) / (self.l * SQRTPI2)
 
     def cdf(self, y, f):
 
-        return self.__split_apply(self._cdf_unif, self._cdf_gaus, y, f)
+        P = self.__split_apply(self._cdf_y_lt_f,
+                               self._cdf_y_gt_f,
+                               self._cdf_f_lt_0,
+                               y, f)
 
-    def _cdf_unif(self, y, f, g):
+        return P
 
-        return y / (g + SQRTPI2)
+    def _cdf_y_lt_f(self, y, f):
 
-    def _cdf_gaus(self, y, f, g):
+        return y / (f + self.l * SQRTPI2)
 
-        return (erf(y - g) * SQRTPI2 + g) / (g + SQRTPI2)
+    def _cdf_y_gt_f(self, y, f):
+
+        return (erf((y - f) / (self.l * np.sqrt(2))) * self.l * SQRTPI2 + f) \
+            / (f + self.l * SQRTPI2)
+
+    def _cdf_f_lt_0(self, y, f):
+
+        return erf((y - f) / (self.l * np.sqrt(2)))
 
     def Ey(self, f):
 
-        g = softplus(f)
+        maxf0 = np.maximum(0, f)
+        y_gt_f_mean = f + self.l / SQRTPI2
+        y_lt_f_mean = maxf0 / 2
 
-        gaus_mean = g + np.sqrt(2 / np.pi)
-        unif_mean = g / 2
+        norm = 1 / (maxf0 + self.l * SQRTPI2)
+        weight_y_gt_f = norm * self.l * SQRTPI2
+        weight_y_lt_f = maxf0 * norm
 
-        norm = 1 / (g + SQRTPI2)
-        weight_gaus = norm * SQRTPI2
-        weight_unif = g * norm
-
-        Ey = weight_gaus * gaus_mean + weight_unif + unif_mean
+        Ey = weight_y_gt_f * y_gt_f_mean + weight_y_lt_f + y_lt_f_mean
 
         return Ey
 
     def df(self, y, f):
 
-        return self.__split_apply(self._df_unif, self._df_gaus, y, f)
+        df = self.__split_apply(self._df_y_lt_f,
+                                self._df_y_gt_f,
+                                self._df_f_lt_0,
+                                y, f)
 
-    def _df_unif(self, y, f, g, gdivs=False):
+        return df
 
-        gp = expit(f)
-        df = - gp / (SQRTPI2 + g)
+    def _df_y_lt_f(self, y, f):
 
-        return df if not gdivs else (df, gp)
+        df = - 1. / (f + self.l * SQRTPI2)
+        return df
 
-    def _df_gaus(self, y, f, g):
+    def _df_y_gt_f(self, y, f):
 
-        gp = expit(f)
-        dnorm, gp = self._df_unif(y, f, g, gdivs=True)
-        return dnorm + 2 * gp * (y - g)
+        df = self._df_y_lt_f(y, f) + self._df_f_lt_0(y, f)
+        return df
+
+    def _df_f_lt_0(self, y, f):
+
+        df = (y - f) / self.l**2
+        return df
 
     def d2f(self, y, f):
 
-        return self.__split_apply(self._d2f_unif, self._d2f_gaus, y, f)
+        return self.__split_apply(self._d2f_y_lt_f,
+                                  self._d2f_y_gt_f,
+                                  self._d2f_f_lt_0,
+                                  y, f)
 
-    def _d2f_unif(self, y, f, g, gdivs=False):
+    def _d2f_y_lt_f(self, y, f):
 
-        gp = expit(f)
-        g2p = gp * (1 - gp)
-        SQRTPI2g = (SQRTPI2 + g)
-        d2f = (gp / SQRTPI2g)**2 - g2p / SQRTPI2g
+        d2f = 1. / (f + self.l * SQRTPI2)**2
+        return d2f
 
-        return d2f if not gdivs else (d2f, gp, g2p)
+    def _d2f_y_gt_f(self, y, f):
 
-    def _d2f_gaus(self, y, f, g):
+        d2f = self._d2f_y_lt_f(y, f) + self._d2f_f_lt_0(y, f)
+        return d2f
 
-        d2norm, gp, g2p = self._d2f_unif(y, f, g, gdivs=True)
-        return d2norm + 2 * (g2p * (y - g) - gp**2)
+    def _d2f_f_lt_0(self, y, f):
+
+        d2f = - 1. / self.l**2
+        return d2f
 
     def d3f(self, y, f):
 
-        return self.__split_apply(self._d3f_unif, self._d3f_gaus, y, f)
+        return self.__split_apply(self._d3f_y_lt_f,
+                                  self._d3f_y_gt_f,
+                                  self._d3f_f_lt_0,
+                                  y, f)
 
-    def _d3f_unif(self, y, f, g, gdivs=False):
+    def _d3f_y_lt_f(self, y, f):
 
-        gp = expit(f)
-        g2p = gp * (1 - gp)
-        g3p = g2p * (1 - 2 * gp)
-        SQRTPI2g = (SQRTPI2 + g)
-        d3f = - g3p / SQRTPI2g + 3 * gp * g2p / SQRTPI2g**2 \
-            - 2 * (gp / SQRTPI2g)**3
+        d3f = - 2. / (f + self.l * SQRTPI2)**3
+        return d3f
 
-        return d3f if not gdivs else (d3f, gp, g2p, g3p)
+    def _d3f_y_gt_f(self, y, f):
 
-    def _d3f_gaus(self, y, f, g):
+        d3f = self._d3f_y_lt_f(y, f)
+        return d3f
 
-        d3norm, gp, g2p, g3p = self._d3f_unif(y, f, g, gdivs=True)
-        d3f_gaus = d3norm + 2 * g3p * (y - g) - 6 * g2p * gp
-        return d3f_gaus
+    def _d3f_f_lt_0(self, y, f):
 
-    def __split_apply(self, func_unif, func_gaus, y, f):
+        df = np.zeros_like(f)
+        return df
+
+    def __split_apply(self, func_y_lt_f, func_y_gt_f, func_f_lt_0, y, f):
 
         # Make sure all arrays are of a compatible type
         y, f = np.broadcast_arrays(y, f)
         y = y.astype(float, copy=False)
         f = f.astype(float, copy=False)
 
+        if any(y < 0):
+            raise ValueError("y has to be > 0!")
+
         # get indicators of which likelihoods to apply where
-        g = softplus(f)
-        isunif = y <= g
-        isgaus = ~ isunif
+        y_lt_f = np.logical_and(y <= f, f > 0)
+        y_gt_f = np.logical_and(y > f, f > 0)
+        f_lt_0 = f <= 0
 
         result = np.zeros_like(y)
 
-        result[isunif] = func_unif(y[isunif], f[isunif], g[isunif])
-        result[isgaus] = func_gaus(y[isgaus], f[isgaus], g[isgaus])
+        result[y_lt_f] = func_y_lt_f(y[y_lt_f], f[y_lt_f])
+        result[y_gt_f] = func_y_gt_f(y[y_gt_f], f[y_gt_f])
+        result[f_lt_0] = func_f_lt_0(y[f_lt_0], f[f_lt_0])
 
         return result
