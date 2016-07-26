29c606a1d820e5351c9dbe4e6bfd580259d7fc3c
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2016-07-26 13:37:17 +1000

    prediction running but strange outputs

--- uncoverml/scripts/predictionpipeline.py
+++ uncoverml/scripts/predictionpipeline.py
@@ -37,10 +37,10 @@ def run_pipeline(config):
         log.info("Made prediction dir")
     comm.barrier()
 
-    # # Make sure prediction dir is empty
-    # if listdir(config.pred_dir):
-    #     log.fatal("Prediction directory must be empty!")
-    #     sys.exit(-1)
+    # Make sure prediction dir is empty
+    if listdir(config.pred_dir):
+        log.fatal("Prediction directory must be empty!")
+        sys.exit(-1)
 
     # Make sure we have an extractfeats settings file for each tif
     tifs = glob(path.join(config.data_dir, "*.tif"))
@@ -50,7 +50,7 @@ def run_pipeline(config):
 
     settings = []
     for tif in tifs:
-        setting = path.join(config.proc_dir, 
+        setting = path.join(config.proc_dir,
                             path.splitext(path.basename(tif))[0] +
                             "_settings.bin")
         if not path.exists(setting):
@@ -61,17 +61,17 @@ def run_pipeline(config):
     # Now extact features from each tif
     ctx = Context(extractfeats)
 
-    # # Find all of the tifs and extract features
-    # for tif, setting in zip(tifs, settings):
-    #     name = path.splitext(path.basename(tif))[0]
-    #     log.info("Processing {}.".format(path.basename(tif)))
-    #     ctx.forward(extractfeats,
-    #                 geotiff=tif,
-    #                 name=name,
-    #                 outputdir=config.pred_dir,
-    #                 config=setting
-    #                 )
-    #     comm.barrier()
+    # Find all of the tifs and extract features
+    for tif, setting in zip(tifs, settings):
+        name = path.splitext(path.basename(tif))[0]
+        log.info("Processing {}.".format(path.basename(tif)))
+        ctx.forward(extractfeats,
+                    geotiff=tif,
+                    name=name,
+                    outputdir=config.pred_dir,
+                    config=setting
+                    )
+        comm.barrier()
 
     # Compose individual image features into single feature vector
     compos_settings = path.join(config.proc_dir,
@@ -124,6 +124,7 @@ def run_pipeline(config):
     ctx.forward(exportgeotiff,
                 name=config.gtiffname + "_" + alg,
                 outputdir=config.pred_dir,
+                imagelike=tifs[0],
                 rgb=config.makergbtif,
                 files=pfiles
                 )
