5a85d24804193e636c2190628b776dd3d3e98d12
Sudipta Basak <basaks@gmail.com>
2016-07-26 23:09:59 +1000

    mask applied. TODO: address large memory usage

--- utils/crop_and_reproject.py
+++ utils/crop_and_reproject.py
@@ -11,7 +11,8 @@ from osgeo import gdal
 
 TSRS = 'EPSG:3112'
 EXTENTS = [str(s) for s in
-          [-2362974.47956, -5097641.80634, 2251415.52044, -1174811.80634]]
+            [-2362974.47956, -5097641.80634, -2362000.47956, -5097000.80634]]
+          # [-2362974.47956, -5097641.80634, 2251415.52044, -1174811.80634]]
 OUTPUT_RES = [str(s) for s in [90, 90]]
 
 
@@ -41,6 +42,21 @@ def crop_resample(input_file, output_file, sampling):
           ['--config', 'GDAL_CACHEMAX', '150']
     cmd += [input_file, output_file]
     subprocess.check_call(cmd)
+
+
+def mask_data(output_tif, mask_file):
+    mask_ds = gdal.Open(mask_file, gdal.GA_ReadOnly)
+    mask_band = mask_ds.GetRasterBand(1)
+    mask_no_data_value = mask_band.GetNoDataValue()
+    masked_data = mask_band.ReadAsArray()
+    mask_array = masked_data == mask_no_data_value
+    mask_ds = None
+    out_ds = gdal.Open(output_tif, gdal.GA_Update)
+    out_band = out_ds.GetRasterBand(1)
+    out_data = out_band.ReadAsArray()
+    out_data[mask_array] = mask_no_data_value
+    out_band.WriteArray(out_data)
+    out_ds = None  # flush data to cache
 
 
 if __name__ == '__main__':
@@ -75,5 +91,7 @@ if __name__ == '__main__':
                   output_file=options.output_file,
                   sampling=options.sampling)
 
+    if options.mask_file:  # if filename is not given
+        mask_data(options.output_file, options.mask_file)
 
 
