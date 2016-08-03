233ee422d63502609de543b16c20f24669c6f224
sudipta <basaks@gmail.com>
2016-08-04 09:00:43 +1000

    nci scripts that work for the national dataset at 90m resolution

--- utils/nci_batch.sh
+++ utils/nci_batch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #PBS -P ge3
 #PBS -q express
-#PBS -l walltime=00:30:00,mem=64GB,ncpus=4,jobfs=200GB
+#PBS -l walltime=00:30:00,mem=64GB,ncpus=3,jobfs=200GB
 #PBS -l wd
 
 module rm intel-fc intel-cc
@@ -9,5 +9,7 @@ module load python/2.7.6
 module load python/2.7.6-matplotlib
 module load gdal/1.11.1-python
 module load openmpi/1.8
+# the uncover-ml directory should be in PYTHONPATH
 export PYTHONPATH=/g/data/ge3/sudipta/uncover-ml:$PYTHONPATH
-mpirun -np 4 python utils/batch_job.py -i ../../covariates/national/ -o ../../covariates/national_LCC/ -m ../../covariates/masks/model_mask.tif -r bilinear -e '-2362974.47956 -5097641.80634 2251415.52044 -1174811.80634'
+# the python script needs full path of the python script
+mpirun -np 3 python utils/batch_job.py -i ../../covariates/national/ -o ../../covariates/national_LCC/ -m ../../covariates/masks/model_mask.tif -r bilinear -e '-2362974.47956 -5097641.80634 2251415.52044 -1174811.80634'
--- utils/nci_single.sh
+++ utils/nci_single.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 #PBS -P ge3
 #PBS -q express
-#PBS -l walltime=00:30:00,mem=16GB,ncpus=1,jobfs=10GB
+#PBS -l walltime=00:30:00,mem=20GB,ncpus=1,jobfs=20GB
 #PBS -l wd
 
 module rm intel-fc intel-cc
 module load python/2.7.6
 module load python/2.7.6-matplotlib
 module load gdal/1.11.1-python
-module load openmpi/1.8
-
+# the python script needs full path of the python script
 python crop_mask_resample_reproject.py -i slope_fill2.tif -o slope_fill2_out.tif  -m mack_LCC.tif -r bilinear -e '-821597 -4530418 1431287 -4174316'
