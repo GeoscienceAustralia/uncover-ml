911a558855c51946b483ea61aeaa2ace4a269eaa
Lachlan McCalman <lachlan.mccalman@data61.csiro.au>
2016-09-20 09:28:34 +1000

    simplify learning on node 0

--- uncoverml/learn.py
+++ uncoverml/learn.py
@@ -1,18 +1,17 @@
 import logging
 
 from uncoverml import mpiops
-from uncoverml import geoio
-from uncoverml import features
 from uncoverml.models import modelmaps, apply_multiple_masked
 
 log = logging.getLogger(__name__)
 
 
 def local_learn_model(x_all, targets_all, config):
-    model = modelmaps[config.algorithm](**config.algorithm_args)
-    y = targets_all.observations
 
+    model = None
     if mpiops.chunk_index == 0:
+        y = targets_all.observations
+        model = modelmaps[config.algorithm](**config.algorithm_args)
         apply_multiple_masked(model.fit, (x_all, y),
                               kwargs={'fields': targets_all.fields})
     model = mpiops.comm.bcast(model, root=0)
