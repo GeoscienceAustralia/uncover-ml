d2182fce36260d52d9b59ecdfbbb62677a476c6d
sudipta <basaks@gmail.com>
2016-09-21 08:33:30 +1000

    more refactoring

--- preprocessing/geoinfo.py
+++ preprocessing/geoinfo.py
@@ -27,20 +27,24 @@ def inspect(input_dir, report_file):
         writer.writerow(['FineName', 'NoDataValue',
                         'rows', 'cols', 'Max', 'Min', 'Mean', 'Std'])
         for t in tifs:
-            ds = gdal.Open(t, gdal.GA_ReadOnly)
-            if ds.RasterCount == 3:
-                log.info('Found multibanded geotif {}'.format(basename(t)))
-                log.info('Please inspect bands individually')
-            else:
-                log.info('Found single band geotif {}'.format(basename(t)))
-            band = ds.GetRasterBand(1)
-            no_data_val = band.GetNoDataValue()
-            l = get_stats(ds, no_data_val, t)
-            writer.writerow([str(a) for a in l])
+            write_rows(t, writer)
 
 
-def get_stats(ds, no_data_val, t):
+def write_rows(t, writer):
+    ds = gdal.Open(t, gdal.GA_ReadOnly)
+    if ds.RasterCount == 3:
+        log.info('Found multibanded geotif {}'.format(basename(t)))
+        log.info('Please inspect bands individually')
+    else:
+        log.info('Found single band geotif {}'.format(basename(t)))
+    l = get_stats(ds, t)
+    writer.writerow([str(a) for a in l])
+
+
+def get_stats(ds, t):
     data = ds.ReadAsArray()
+    band = ds.GetRasterBand(1)
+    no_data_val = band.GetNoDataValue()
     l = [basename(t), no_data_val,
          ds.RasterYSize, ds.RasterXSize,
          np.max(data), np.min(data),
