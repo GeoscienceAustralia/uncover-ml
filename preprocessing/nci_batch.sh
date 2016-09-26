66122989ee264829d1cffa4c984d57fbdd6ee474
sudipta <basaks@gmail.com>
2016-09-26 18:41:07 +1000

    use python3 uncovelml virtualenv

--- preprocessing/nci_batch.sh
+++ preprocessing/nci_batch.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 #PBS -P ge3
-#PBS -q express
-#PBS -l walltime=00:30:00,mem=64GB,ncpus=3,jobfs=200GB
+#PBS -q normal
+#PBS -l walltime=20:00:00,mem=64GB,ncpus=16,jobfs=30GB
 #PBS -l wd
 
-module rm intel-fc intel-cc
-module load python/2.7.6
-module load python/2.7.6-matplotlib
-module load gdal/1.11.1-python
+module unload intel-cc
+module unload intel-fc
+module load python3/3.4.3 python3/3.4.3-matplotlib
+module load hdf5/1.8.10 gdal/2.0.0
 module load openmpi/1.8
-# the uncover-ml directory should be in PYTHONPATH
-export PYTHONPATH=/g/data/ge3/sudipta/uncover-ml:$PYTHONPATH
+
+export PATH=$HOME/.local/bin:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.4/site-packages:$PYTHONPATH
+export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.4.3/bin/python3
+export LC_ALL=en_AU.UTF-8
+export LANG=en_AU.UTF-8
+source $HOME/.local/bin/virtualenvwrapper.sh
+
+# start the virtualenv
+workon uncoverml
+
 # the python command needs full path of the python script
-mpirun -np 3 python preprocessing/batch_job.py -i ../../covariates/national/ -o ../../covariates/national_LCC/ -m ../../covariates/masks/model_mask.tif -r bilinear -e '-2362974.47956 -5097641.80634 2251415.52044 -1174811.80634'
+mpirun -np 4 python batch_job.py -i /g/data/ge3/covariates/national -o /g/data/ge3/covariates/national_masked -m /g/data/ge3/covariates/masks/Mask_National_LL_nan_to_keep.tif -r bilinear
--- preprocessing/nci_single.sh
+++ preprocessing/nci_single.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 #PBS -P ge3
 #PBS -q express
-#PBS -l walltime=00:30:00,mem=20GB,ncpus=1,jobfs=20GB
+#PBS -l walltime=00:20:00,mem=20GB,ncpus=1,jobfs=30GB
 #PBS -l wd
 
-module rm intel-fc intel-cc
-module load python/2.7.6
-module load python/2.7.6-matplotlib
-module load gdal/1.11.1-python
+module unload intel-cc
+module unload intel-fc
+module load python3/3.4.3 python3/3.4.3-matplotlib
+module load hdf5/1.8.10 gdal/2.0.0
+module load openmpi/1.8
+
+export PATH=$HOME/.local/bin:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.4/site-packages:$PYTHONPATH
+export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.4.3/bin/python3
+export LC_ALL=en_AU.UTF-8
+export LANG=en_AU.UTF-8
+source $HOME/.local/bin/virtualenvwrapper.sh
+
+# start the virtualenv
+workon uncoverml
+
 # the python command needs full path of the python script
 python preprocessing/crop_mask_resample_reproject.py -i slope_fill2.tif -o slope_fill2_out.tif  -m mack_LCC.tif -r bilinear -e '-821597 -4530418 1431287 -4174316'
