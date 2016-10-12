980049f0d49d93c24a2f69a7cfe9d9f7c49cd372
sudipta <basaks@gmail.com>
2016-10-12 12:33:48 +1100

    added mask_no_data functionality tests

--- tests/test_average.py
+++ tests/test_average.py
@@ -80,6 +80,63 @@ class TestUniformFilterWithNoData(unittest.TestCase):
                                              self.expected_average_5)
 
 
+class TestFilterCenterWithNoDataAndMask(unittest.TestCase):
+
+    def setUp(self):
+        self.data = np.array([[1000.0, 1.2, 1.4, 1.6, 1.8],
+                              [1000.0, 1.2, 1.4, 1.6, 1.8],
+                              [1.0, 1.2, 1000.0, 1.6, 1.8],
+                              [2.0, 1.2, 1.4, 1.6, 1.8],
+                              [1000.0, 1.2, 1.4, 1.6, 1.8]])
+
+        self.expected_average_3 = np.array([[1000.0, 1.3, 1.4, 1.6, 1.7],
+                                            [1000.0, 1.23333333, 1.4, 1.625, 1.7],
+                                            [1.32, 1.34285714, 1000.0, 1.625, 1.7],
+                                            [1.32, 1.34285714, 1.4, 1.625, 1.7],
+                                            [1000.0, 1.44, 1.4, 1.6, 1.7]])
+
+        self.expected_average_5 = np.array(
+            [[1000.0, 1.35555556, 1.46666667, 1.50909091, 1.625],
+             [1000.0, 1.41538462, 1.50588235, 1.50666667, 1.61818182],
+             [1.32727273, 1.4125, 1000.0, 1.50526316, 1.61428571],
+             [1.33333333, 1.41538462, 1.50588235, 1.50666667, 1.61818182],
+             [1000.0, 1.42, 1.50769231, 1.50909091, 1.625]]
+            )
+
+        self.data_5x4 = np.array([[1000.0, 1.2, 1.4, 1.6],
+                              [1000.0, 1.2, 1.4, 1.6],
+                              [1.0, 1.2, 1000.0, 1.6],
+                              [2.0, 1.2, 1.4, 1.6],
+                              [1000.0, 1.2, 1.4, 1.6]])
+        self.expected_average_3_5x4 = np.array([[1000.0, 1.3, 1.4, 1.5],
+                                            [1000.0, 1.23333333, 1.4, 1.52],
+                                            [1.32, 1.34285714, 1000.0, 1.52],
+                                            [1.32, 1.34285714, 1.4, 1.52],
+                                            [1000.0, 1.44, 1.4, 1.5]])
+
+    def test_average_size3(self):
+        averaged_data = raster_average.filter_center(
+            self.data, size=3, no_data_val=1000.0, func=np.nanmean,
+            mask_no_data=True)
+        np.testing.assert_array_almost_equal(averaged_data,
+                                             self.expected_average_3)
+
+    def test_average_size3_5x4(self):
+        averaged_data = raster_average.filter_center(
+            self.data_5x4, size=3, no_data_val=1000.0, func=np.nanmean,
+            mask_no_data=True)
+
+        np.testing.assert_array_almost_equal(averaged_data,
+                                             self.expected_average_3_5x4)
+
+    def test_average_size5(self):
+        averaged_data = raster_average.filter_center(
+            self.data, size=5, no_data_val=1000.0, func=np.nanmean,
+            mask_no_data=True)
+        np.testing.assert_array_almost_equal(averaged_data,
+                                             self.expected_average_5)
+
+
 class TestFilterCenterWithNoData(unittest.TestCase):
 
     def setUp(self):
