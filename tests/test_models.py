04c517e5a1237b8b59af1ef828390cb10910b27c
sudipta <basaks@gmail.com>
2016-12-11 20:57:41 +1100

    new tests for optimise.models classes

--- tests/test_models.py
+++ tests/test_models.py
@@ -64,6 +64,20 @@ def test_krige(linear_data, get_krige_method):
     Ey = mod.predict(np.tile(Xs, (1, 2)))
     assert r2_score(ys, Ey) > 0
 
+
+@pytest.fixture(params=[k for k in transformed_modelmaps])
+def get_transformed_model(request):
+    return transformed_modelmaps[request.param]
+
+
+def test_ml_score(get_transformed_model):
+    """
+    make sure all optimise.models classes have ml_score attr
+    """
+    assert np.all([hasattr(get_transformed_model(), a) for a in
+                   ['ml_score', 'score', 'fit', 'predict']])
+
+
 # def test_modelpersistance(make_fakedata):
 
 #     X, y, _, mod_dir = make_fakedata
