6f642e544381993a83f98ccd3a960322f99e2d4a
Sudipta Basak <basaks@gmail.com>
2018-03-04 10:20:23 +1100

    remaining fixes for test_preprocessing

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,99 +1,102 @@
 import os
 import shutil
-import tempfile
 from os.path import join, basename, exists
-
+import pytest
 from osgeo import gdal
 from preprocessing import crop_mask_resample_reproject as crop
 from preprocessing.crop_mask_resample_reproject import Options
 
+# The mock files have extents = '900000 -4300000 940000 -4260000'
+mock_extents = [920000, -4300000, 930000, -4280000]
 
-class TestCropReSampleReProject:
 
-    @classmethod
-    def setup_class(cls, uncover):
-        mocks = join(uncover, 'preprocessing', 'mocks')
-        # These file shave extents = '900000 -4300000 940000 -4260000'
-        cls.mask = join(mocks, 'mask.tif')
-        cls.std2000 = join(mocks, 'std2000.tif')
-        cls.std2000_no_mask = join(mocks, 'std2000_no_mask.tif')
-        cls.extents = [920000, -4300000, 930000, -4280000]
+@pytest.fixture
+def mock_files(uncover):
+    data_dir = os.path.join(uncover, 'preprocessing', 'mocks')
+    std2000 = os.path.join(data_dir, 'std2000.tif')
+    std2000_no_mask = os.path.join(data_dir, 'std2000_no_mask.tif')
+    mask = os.path.join(data_dir, 'mask.tif')
+    result = dict(std2000=std2000, mask=mask,
+                  std2000_no_mask=std2000_no_mask)
+    return result
 
-    def test_geotransform_projection_nodata(self):
-        tmp_output = tempfile.mktemp(suffix='.tif')
-        extents = [str(s) for s in [920000, -4300000, 929000, -4290000]]
 
-        # the mock is already geotransformed, so this will have no effect
-        # to projection and nodata, but will be cropped making the
-        # geotransform tuple different
-        crop.crop_reproject_resample(self.std2000_no_mask, tmp_output,
-                                     sampling='bilinear',
-                                     extents=extents,
-                                     reproject=True)
+def test_geotransform_projection_nodata(mock_files, random_filename):
+    tmp_output = random_filename(ext='.tif')
+    extents = [str(s) for s in [920000, -4300000, 929000, -4290000]]
 
-        ds = gdal.Open(tmp_output)
-        gt = ds.GetGeoTransform()
-        projection = ds.GetProjection()
-        nodata = ds.GetRasterBand(1).GetNoDataValue()
-        ds = None
+    # the mock is already geotransformed, so this will have no effect
+    # to projection and nodata, but will be cropped making the
+    # geotransform tuple different
+    crop.crop_reproject_resample(mock_files['std2000_no_mask'], tmp_output,
+                                 sampling='bilinear',
+                                 extents=extents,
+                                 reproject=True)
 
-        ds = gdal.Open(self.std2000_no_mask)
-        projection_input = ds.GetProjection()
-        nodata_input = ds.GetRasterBand(1).GetNoDataValue()
-        ds = None
+    ds = gdal.Open(tmp_output)
+    gt = ds.GetGeoTransform()
+    projection = ds.GetProjection()
+    nodata = ds.GetRasterBand(1).GetNoDataValue()
+    ds = None
 
-        self.assertEqual(nodata, nodata_input)
-        self.assertEqual(projection, projection_input)
-        self.assertEqual(gt[1], float(crop.OUTPUT_RES[0]))
-        self.assertEqual(gt[0], float(extents[0]))
-        self.assertEqual(gt[3], float(extents[3]))
-        os.remove(tmp_output)
+    ds = gdal.Open(mock_files['std2000_no_mask'])
+    projection_input = ds.GetProjection()
+    nodata_input = ds.GetRasterBand(1).GetNoDataValue()
+    ds = None
 
-    def test_apply_mask(self):
-        output_file = tempfile.mktemp(suffix='.tif')
-        jpeg = False
-        tmp_out_file = tempfile.mktemp(suffix='.tif')
-        shutil.copy(self.std2000_no_mask, tmp_out_file)
-        crop.apply_mask(mask_file=self.mask,
-                        tmp_output_file=tmp_out_file,
-                        output_file=output_file,
-                        jpeg=jpeg)
+    assert nodata == nodata_input
+    assert projection == projection_input
+    assert gt[1] == float(crop.OUTPUT_RES[0])
+    assert gt[0] == float(extents[0])
+    assert gt[3] == float(extents[3])
+    os.remove(tmp_output)
 
-        ds = gdal.Open(output_file)
-        gt = ds.GetGeoTransform()
-        projection = ds.GetProjection()
-        nodata = ds.GetRasterBand(1).GetNoDataValue()
-        ds = None
 
-        ds = gdal.Open(self.std2000)
-        projection_input = ds.GetProjection()
-        nodata_input = ds.GetRasterBand(1).GetNoDataValue()
-        ds = None
+def test_apply_mask(mock_files, random_filename):
+    output_file = random_filename(ext='.tif')
+    jpeg = False
+    tmp_out_file = random_filename(ext='.tif')
+    shutil.copy(mock_files['std2000_no_mask'], tmp_out_file)
+    crop.apply_mask(mask_file=mock_files['mask'],
+                    tmp_output_file=tmp_out_file,
+                    output_file=output_file,
+                    jpeg=jpeg)
 
-        self.assertEqual(nodata, nodata_input)
-        self.assertEqual(projection, projection_input)
-        self.assertEqual(gt[1], float(crop.OUTPUT_RES[0]))
-        self.assertEqual(gt[0], self.extents[0])
-        self.assertEqual(gt[3], self.extents[3])
-        os.remove(output_file)
+    ds = gdal.Open(output_file)
+    gt = ds.GetGeoTransform()
+    projection = ds.GetProjection()
+    nodata = ds.GetRasterBand(1).GetNoDataValue()
+    ds = None
 
-    def test_do_work(self):
-        # input_file, mask_file, output_file, resampling, extents, jpeg
-        output_file = tempfile.mktemp(suffix='.tif')
-        options = Options(resampling='bilinear',
-                          extents=self.extents,
-                          jpeg=True,
-                          reproject=True)
-        crop.do_work(input_file=self.std2000_no_mask,
-                     output_file=output_file,
-                     options=options,
-                     mask_file=self.mask)
+    ds = gdal.Open(mock_files['std2000'])
+    projection_input = ds.GetProjection()
+    nodata_input = ds.GetRasterBand(1).GetNoDataValue()
+    ds = None
 
-        # output file was created
-        self.assertTrue(exists(output_file))
+    assert nodata == nodata_input
+    assert projection == projection_input
+    assert gt[1] == float(crop.OUTPUT_RES[0])
+    assert gt[0] == mock_extents[0]
+    assert gt[3] == mock_extents[3]
+    os.remove(output_file)
 
-        # assert jpeg was created
-        self.assertTrue(exists(
-            join(crop.TMPDIR, basename(output_file).split('.')[0] + '.jpg')))
 
-        os.remove(output_file)
+def test_do_work(mock_files, random_filename):
+    # input_file, mask_file, output_file, resampling, extents, jpeg
+    output_file = random_filename(ext='.tif')
+    options = Options(resampling='bilinear',
+                      extents=mock_extents,
+                      jpeg=True,
+                      reproject=True)
+    crop.do_work(input_file=mock_files['std2000_no_mask'],
+                 output_file=output_file,
+                 options=options,
+                 mask_file=mock_files['mask'])
+
+    # output file was created
+    assert exists(output_file)
+
+    # assert jpeg was created
+    assert exists(output_file.rsplit('.')[0] + '.jpg')
+
+    os.remove(output_file)
