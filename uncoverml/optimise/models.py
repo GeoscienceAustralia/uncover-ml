95e5d26ab802e87dd93754096b1a9966998f15dc
Sudipta Basak <basaks@gmail.com>
2018-03-03 17:38:48 +1100

    need arg and kwargs for pipeline compatibility
    [ci skip]

--- uncoverml/optimise/models.py
+++ uncoverml/optimise/models.py
@@ -281,7 +281,7 @@ class TransformedGPRegressor(TransformMixin, GaussianProcessRegressor,
             random_state=random_state
         )
 
-    def predict_proba(self, X, interval=0.95):
+    def predict_proba(self, X, interval=0.95, *args, **kwargs):
         Ey, std_t = super().predict(X, return_std=True)
         ql, qu = norm.interval(interval, loc=Ey, scale=std_t)
 
