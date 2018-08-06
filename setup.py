8d90b36e4f60df48a89886693e20980282257694
Sudipta Basak <basaks@gmail.com>
2018-08-06 12:20:47 +1000

    fix cubist installation
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,33 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 # If testing in python 2, use subprocess32 instead of built in subprocess
 if os.name == 'posix' and sys.version_info[0] < 3:
-    exta_test_deps = ['subprocess32']
+    extra_test_deps = ['subprocess32']
 else:
-    exta_test_deps = []
-
-
-class PyTest(TestCommand):
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        super(PyTest, self).initialize_options()
-        self.pytest_args = []
-
-    def finalize_options(self):
-        super(PyTest, self).finalize_options()
-        self.test_suite = True
-        self.test_args = []
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        exit(pytest.main(self.pytest_args))
+    extra_test_deps = []
 
 
 readme = open('README.rst').read()
@@ -38,7 +19,31 @@ Documentation
 The full documentation is at http://GeoscienceAustralia.github.io/uncover-ml/."""
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+def build_cubist():
+    try:
+        from uncoverml import cubist_config
+        out = subprocess.run([cubist_config.invocation, '-h'])
+        print(out)
+    except:
+        out = subprocess.run(['./cubist/makecubist', '.'])
+
+
+class CustomInstall(install):
+    def run(self):
+        build_cubist()
+        install.run(self)
+
+class CustomDevelop(develop):
+    def run(self):
+        build_cubist()
+        develop.run(self)
+
 setup(
+    cmdclass={'install': CustomInstall,
+              'develop': CustomDevelop},
     name='uncover-ml',
     version='0.1.0',
     description='Machine learning tools for the Geoscience Australia uncover '
@@ -49,7 +54,7 @@ setup(
     author_email='John.Wilford@ga.gov.au',
     url='https://github.com/GeoscienceAustralia/uncover-ml',
     packages=['uncoverml', 'uncoverml.scripts', 'uncoverml.transforms',
-              'preprocessing', 'uncoverml.optimise'],
+              'uncoverml.optimise'],
     package_dir={'uncover-ml': 'uncoverml'},
     include_package_data=True,
     entry_points={
@@ -58,9 +63,6 @@ setup(
             'gammasensor = uncoverml.scripts.gammasensor:cli',
             'tiff2kmz = uncoverml.scripts.tiff2kmz:main',
             'subsampletargets = uncoverml.scripts.subsampletargets:cli',
-            'geoinfo = preprocessing.geoinfo:cli',
-            'resample = preprocessing.resample:cli',
-            'rasteraverage = preprocessing.raster_average:cli',
             'gridsearch = uncoverml.scripts.gridsearch:cli'
         ]
     },
@@ -82,7 +84,6 @@ setup(
         'PyYAML >= 3.11',
         'GDAL >= 2.0.0',
         'pandas == 0.19.2',
-        'geopandas == 0.2.1',
         'matplotlib >= 1.5.1',
         'PyKrige == 1.3.0',
         'xgboost >= 0.72.1',
@@ -100,18 +101,14 @@ setup(
             'sphinx',
             'ghp-import',
             'sphinxcontrib-programoutput'
-        ]
+            'sphinxcontrib-programoutput',
+                   'pytest',
+                   'pytest-cov',
+                   'coverage',
+                   'codecov',
+                   'tox',
+               ] + extra_test_deps
     },
-    cmdclass={
-        'test': PyTest
-    },
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-        'coverage',
-        'codecov',
-        'tox',
-    ] + exta_test_deps,
     license="Apache Software License 2.0",
     zip_safe=False,
     keywords='uncover-ml',
