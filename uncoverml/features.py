4e409f4e0e03e441a9ed3aec05cfaf90c5da3bb8
Sudipta Basak <basaks@gmail.com>
2019-05-23 06:03:04 +1000

    export target values in csv

--- uncoverml/features.py
+++ uncoverml/features.py
@@ -96,7 +96,7 @@ def transform_features(feature_sets, transform_sets, final_transform, config):
     return x, cull_all_null_rows(feature_sets)
 
 
-def save_intersected_features(feature_sets, transform_sets, config):
+def save_intersected_features_and_targets(feature_sets, transform_sets, targets, config):
     """
     This function saves raw covariates values at the target locations, i.e.,
     after the targets have been intersected.
@@ -113,6 +113,7 @@ def save_intersected_features(feature_sets, transform_sets, config):
              for ec in feature_sets
              for k in ec
              for b in range(ec[k].shape[3])]
+    names.append(config.target_property + "(target)")
 
     header = ', '.join(names)
     for t in transform_sets:
@@ -126,11 +127,16 @@ def save_intersected_features(feature_sets, transform_sets, config):
 
     x = np.ma.concatenate(transformed_vectors, axis=1)
     x_all = gather_features(x, node=0)
+
+    t = np.atleast_2d(targets.observations).T
+
     if mpiops.chunk_index == 0:
-        np.savetxt(config.rawcovariates, X=x_all.data, delimiter=',',
+        data = np.hstack((x_all.data, t))
+        np.savetxt(config.rawcovariates, X=data, delimiter=',',
                    fmt='%.4e',
                    header=header)
-        np.savetxt(config.rawcovariates_mask, X=x_all.mask.astype(int),
+        mask = np.hstack((x_all.mask.astype(int), np.zeros_like(t)))
+        np.savetxt(config.rawcovariates_mask, X=mask,
                    delimiter=',', fmt='%d', header=header)
         if config.plot_covariates:
             import matplotlib.pyplot as plt
