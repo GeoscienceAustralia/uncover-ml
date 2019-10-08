568f56c3de90e7035f9e63f721f411cec0638a24
Bren Moushall <bmoush@gmail.com>
2019-10-08 04:14:46 +0000

    Fix test fixtures

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,8 +1,12 @@
+import pytest
 import numpy as np
 
 from uncoverml import patch
 
 
+@pytest.mark.parametrize('make_multi_patch', 
+                         ['make_patch_31', 'make_patch_11'],
+                         indirect=True)
 def test_grid_patch(make_multi_patch):
 
     timg, pwidth, tpatch, tx, ty = make_multi_patch
