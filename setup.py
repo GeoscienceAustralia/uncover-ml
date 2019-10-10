4e93139374fdb20a9e0fd06e8bcbc0ae48dfde8e
Bren Moushall <bmoush@gmail.com>
2019-10-10 02:59:01 +0000

    Remove tox
    Not in use, was added as part of cookiecutter template

--- setup.py
+++ setup.py
@@ -107,7 +107,6 @@ setup(
             'sphinxcontrib-programoutput==0.15',
             'pytest==5.2.1',
             'pytest-cov==2.8.1',
-            'tox==3.2.1',
             'twine==2.0.0',
             'setuptools==41.4.0',
             'wheel==0.33.6'
