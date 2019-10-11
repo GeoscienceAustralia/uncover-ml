0412c089255198d55c83bcddea7a0e5d40a0044d
Bren Moushall <bmoush@gmail.com>
2019-10-11 05:06:36 +0000

    Fix python version

--- pbs/setup_hpc.sh
+++ pbs/setup_hpc.sh
@@ -8,8 +8,8 @@ module load hdf5/1.8.10
 module load geos/3.5.0
 
 export PATH=$HOME/.local/bin:$PATH
-export PYTHONPATH=$HOME/.local/lib/python3.4/site-packages:$PYTHONPATH
-export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.4.3/bin/python3                 
+export PYTHONPATH=$HOME/.local/lib/python3.7/site-packages:$PYTHONPATH
+export VIRTUALENVWRAPPER_PYTHON=/apps/python3/3.7.2/bin/python3                 
 export LC_ALL=en_AU.UTF-8
 export LANG=en_AU.UTF-8
 source $HOME/.local/bin/virtualenvwrapper.sh 
