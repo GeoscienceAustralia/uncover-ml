fe22984d52006b8f32bf4740118b0efb5536ce39
Bren Moushall <bmoush@gmail.com>
2019-10-23 23:06:32 +0000

    Add learn script test that tests outputs exist
    Former-commit-id: 883d8a131a9187a970d206519fdf096d4aefa2e9

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -8,22 +8,17 @@ import pytest
 from uncoverml.scripts import uncoverml
 
 
-SIRSAM_RF_OUTPUTS = [
+SIRSAM_RF_LEARN_OUTPUTS = [
     'features.pk',
     'targets.pk',
     'rawcovariates.csv',
     'rawcovariates_mask.csv',
-    'sirsam_Na.model',
-    'sirsam_Na_results.csv',
-    'sirsam_Na_results.hdf5',
-    'sirsam_Na_results.png',
-    'sirsam_Na_scores.json',
-    'training_data.pk'
-]
-
-# Until we refactor output file directories, this separate list of top-level directory outputs
-# is required.
-SIRSAM_RF_TOPLEVEL_OUTPUTS = [
+    'sirsam_Na_randomforest.model',
+    'sirsam_Na_randomforest_results.csv',
+    'sirsam_Na_randomforest_results.hdf5',
+    'sirsam_Na_randomforest_results.png',
+    'sirsam_Na_randomforest_scores.json',
+    'training_data.pk',
     '0_Clim_Prescott_LindaGregory.png',
     '0_U_15v1.png',
     '0_U_TH_15.png',
@@ -34,27 +29,24 @@ SIRSAM_RF_TOPLEVEL_OUTPUTS = [
     '0_tpi_300.png'
 ]
 
-@pytest.fixture(scope='module', autouse=True)
-def run_sirsam_random_forest_learning(sirsam_rf_conf):
-    uncoverml.learn([sirsam_rf_conf, '-p', 20])
+class TestLearnCommand:
+    @pytest.fixture(scope='class', autouse=True)
+    def run_sirsam_random_forest_learning(self, request, sirsam_rf_conf, sirsam_rf_out):
+        def finalize():
+            shutil.rmtree(sirsam_rf_out)
 
-@pytest.fixture(params=SIRSAM_RF_OUTPUTS)
-def sirsam_rf_output(request, sirsam_rf_out):
-    return os.path.join(sirsam_rf_out, request.param)
+        request.addfinalizer(finalize)
 
-# Todo: consolidate with above fixture after output refactor
-@pytest.fixture(params=SIRSAM_RF_TOPLEVEL_OUTPUTS)
-def sirsam_rf_toplevel_output(request, sirsam_rf):
-    return os.path.join(sirsam_rf, request.param)
+        try:
+            return uncoverml.learn([sirsam_rf_conf, '-p', 20])
+        # Catch SystemExit as it gets raised by Click on command competion
+        except SystemExit:
+            pass
+        
+    @pytest.fixture(params=SIRSAM_RF_LEARN_OUTPUTS)
+    def sirsam_rf_output(self, request, sirsam_rf_out):
+        return os.path.join(sirsam_rf_out, request.param)
 
-def test_output_exists(sirsam_rf_output):
-    assert os.path.exists(sirsam_rf_output)
-
-# Todo: consolidate with above test after output refactor
-def test_toplevel_output_exists(sirsam_rf_toplevel_output):
-    assert os.path.exists(sirsam_rf_toplevel_output)
-
-
-
-
-
+    def test_output_exists(self, sirsam_rf_output):
+        print(f"testing that '{sirsam_rf_output}' exists...")
+        assert os.path.exists(sirsam_rf_output)
