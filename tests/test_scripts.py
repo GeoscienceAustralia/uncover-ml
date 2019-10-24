2dd50a7353682a2dc1678c74c6556053effcb459
Bren Moushall <bmoush@gmail.com>
2019-10-24 06:59:33 +0000

    Add test for comparing generated model
    Former-commit-id: da91823d8e7df386941fa0141ee73dd52ca96211

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -3,6 +3,7 @@ import sys
 import glob
 import shutil
 import csv
+import pickle
 
 import pytest
 
@@ -10,13 +11,16 @@ from uncoverml.scripts import uncoverml
 
 
 class TestLearnCommand:
+    SIRSAM_RF = 'sirsam_Na_randomforest'
+
+    SIRSAM_RF_MODEL = SIRSAM_RF + '.model'
+
     SIRSAM_RF_LEARN_OUTPUT = [
-        'rawcovariates.csv',
-        'rawcovariates_mask.csv',
-        'sirsam_Na_randomforest.model',
-        'sirsam_Na_randomforest_results.csv',
-        'sirsam_Na_randomforest_results.hdf5',
-        'sirsam_Na_randomforest_results.png',
+        SIRSAM_RF_MODEL,
+        SIRSAM_RF + '_results.csv',
+        SIRSAM_RF + '_results.hdf5',
+        SIRSAM_RF + '_results.png',
+        SIRSAM_RF + '_scores.json'
     ]
 
     SIRSAM_RF_PICKLE_DATA = [
@@ -45,7 +49,7 @@ class TestLearnCommand:
                         + SIRSAM_RF_PICKLE_DATA \
                         + SIRSAM_RF_COVARIATE_OUTPUT \
                         + SIRSAM_RF_COVARIATE_PLOTS
-
+    
     @staticmethod
     @pytest.fixture(scope='class', autouse=True)
     def run_sirsam_random_forest_learning(request, sirsam_rf_conf, sirsam_rf_out):
@@ -87,9 +91,27 @@ class TestLearnCommand:
 
     @staticmethod
     def test_covariate_csv_outputs_match(sirsam_rf_covariate_outputs):
+        """ 
+        Test that CSV covariate info matches precomputed output.
+        """
         with open(sirsam_rf_covariate_outputs[0]) as test, \
                 open(sirsam_rf_covariate_outputs[1]) as precomp:
             test_lines = test.readlines()
             precomp_lines = precomp.readlines()
-        assert test_lines == precomp_lines  
-        
+        assert test_lines == precomp_lines
+
+    @staticmethod
+    def test_model_outputs_match(sirsam_rf_out, sirsam_rf_precomp):
+        """
+        Test that generated model matches precomputed output.
+        """
+        test_model_path = os.path.join(sirsam_rf_out, TestLearnCommand.SIRSAM_RF_MODEL)
+        precomp_model_path = \
+            os.path.join(sirsam_rf_precomp, 'learn_outputs', TestLearnCommand.SIRSAM_RF_MODEL)
+        with open(test_model_path, 'rb') as tm_pk, open(precomp_model_path, 'rb') as pc_pk:
+            test_model, test_config = pickle.load(tm_pk)
+            precomp_model, precomp_config = pickle.load(pc_pk)
+        assert test_model == precomp_model
+        assert test_config == precomp_config
+
+    @staticmethod
