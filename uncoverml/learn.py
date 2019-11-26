Fix parallel r/w issue with target scaling
Former-commit-id: 6e678058d49006ea9415b453c1402e1e0c530335

uncoverml/learn.py
@@ -29,7 +29,7 @@ def local_learn_model(x_all, targets_all, config):
             mpiops.comm.barrier()
             if mpiops.chunk_index == 0:
                 for rf in rf_dicts:
-                    model._randomforests.update(rf)        
+                    model._randomforests.update(rf)
     else:
         if mpiops.chunk_index == 0:
             y = targets_all.observations
@@ -39,16 +39,16 @@ def local_learn_model(x_all, targets_all, config):
                                           'lon_lat': targets_all.positions})
 
     # Save transformed targets for diagnostics
-    if hasattr(model, 'target_transform'):
+    if mpiops.chunk_index == 0 and hasattr(model, 'target_transform'):
         hdr = 'nontransformed,transformed'
         y = targets_all.observations
         y_t = model.target_transform.transform(y)
         np.savetxt(config.transformed_targets_file, X=np.column_stack((y, y_t)),
                    delimiter=',', header=hdr, fmt='%.4e')
 
-    if config.plot_target_scaling:
-        diagnostics.plot_target_scaling(
-            config.transformed_targets_file).savefig(config.plot_target_scaling)
+        if config.plot_target_scaling:
+            diagnostics.plot_target_scaling(
+                config.transformed_targets_file).savefig(config.plot_target_scaling)
         
 
     return model