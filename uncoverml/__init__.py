4d09ee402bf877c1b19cdb07bdea677b532b8b04
Bren Moushall <bmoush@gmail.com>
2019-12-11 05:06:10 +0000

    Remove old configs, add sanity test command to install docs

--- uncoverml/__init__.py
+++ uncoverml/__init__.py
@@ -1,7 +1,13 @@
+import os
 import pkg_resources
+
+import uncoverml
 
 __author__ = 'Geoscience Australia Mineral Systems Group, ' \
              'NICTA Spatial Inference Systems Team'
 __email__ = 'daniel.steinberg@nicta.com.au, basaks@gmail.com'
 __version__ = pkg_resources.get_distribution('uncover-ml').version
+
+if 'UNCOVERML_SRC' not in os.environ:
+    os.environ['UNCOVERML_SRC'] = os.path.split(uncoverml.__path__[0])[0]
 
