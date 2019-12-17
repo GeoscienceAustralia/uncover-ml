d2a922be259058c5f99091e42799beb4fd612a3a
Bren Moushall <bmoush@gmail.com>
2019-12-17 05:19:14 +0000

    Fix target y_min/y_max depending on ordering

--- uncoverml/scripts/uncoverml.py
+++ uncoverml/scripts/uncoverml.py
@@ -73,9 +73,9 @@ def shiftmap(config_file, partitions):
     image_chunk_sets = ls.geoio.image_feature_sets(targets, config)
     transform_sets = [k.transform_set for k in config.feature_sets]
     features, keep = ls.features.transform_features(image_chunk_sets,
-                                                     transform_sets,
-                                                     config.final_transform,
-                                                     config)
+                                                    transform_sets,
+                                                    config.final_transform,
+                                                    config)
     x_all = ls.features.gather_features(features[keep], node=0)
     targets_all = ls.targets.gather_targets(targets, keep, config, node=0)
     model = ls.models.LogisticClassifier(random_state=1)
@@ -136,70 +136,70 @@ def learn(config_file, partitions):
 
     _logger.info("Finished! Total mem = {:.1f} GB".format(_total_gb()))
 
-    def _load_data(config, partitions):
-        if config.pk_load:
-            x_all = pickle.load(open(config.pk_covariates, 'rb'))
-            targets_all = pickle.load(open(config.pk_targets, 'rb'))
-            if config.cubist or config.multicubist:
-                config.algorithm_args['feature_type'] = \
-                    pickle.load(open(config.pk_featurevec, 'rb'))
-            _logger.warning("Using  pickled targets and covariates. Make sure you have"
-                            " not changed targets file and/or covariates.")
+def _load_data(config, partitions):
+    if config.pk_load:
+        x_all = pickle.load(open(config.pk_covariates, 'rb'))
+        targets_all = pickle.load(open(config.pk_targets, 'rb'))
+        if config.cubist or config.multicubist:
+            config.algorithm_args['feature_type'] = \
+                pickle.load(open(config.pk_featurevec, 'rb'))
+        _logger.warning("Using  pickled targets and covariates. Make sure you have"
+                        " not changed targets file and/or covariates.")
+    else:
+        if config.crop_box:
+            ls.geoio.crop_covariates(config)
+        config.n_subchunks = partitions
+        if config.n_subchunks > 1:
+            _logger.info("Memory constraint forcing {} iterations "
+                         "through data".format(config.n_subchunks))
         else:
-            if config.crop_box:
-                ls.geoio.crop_covariates(config)
-            config.n_subchunks = partitions
-            if config.n_subchunks > 1:
-                _logger.info("Memory constraint forcing {} iterations "
-                             "through data".format(config.n_subchunks))
-            else:
-                _logger.info("Using memory aggressively: "
-                             "dividing all data between nodes")
+            _logger.info("Using memory aggressively: "
+                         "dividing all data between nodes")
 
-            # Make the targets
-            _logger.info("Intersecting targets as pickled train data was not "
-                         "available")
-            targets = ls.geoio.load_targets(shapefile=config.target_file,
-                                            targetfield=config.target_property,
-                                            covariate_crs=ls.geoio.get_image_crs(config),
-                                            crop_box=config.crop_box)
-                                                
-            # Get the image chunks and their associated transforms
-            image_chunk_sets = ls.geoio.image_feature_sets(targets, config)
-            transform_sets = [k.transform_set for k in config.feature_sets]
+        # Make the targets
+        _logger.info("Intersecting targets as pickled train data was not "
+                     "available")
+        targets = ls.geoio.load_targets(shapefile=config.target_file,
+                                        targetfield=config.target_property,
+                                        covariate_crs=ls.geoio.get_image_crs(config),
+                                        crop_box=config.crop_box)
+                                            
+        # Get the image chunks and their associated transforms
+        image_chunk_sets = ls.geoio.image_feature_sets(targets, config)
+        transform_sets = [k.transform_set for k in config.feature_sets]
 
-            if config.raw_covariates:
-                _logger.info("Saving raw data before any processing")
-                ls.features.save_intersected_features_and_targets(image_chunk_sets,
-                                                                  transform_sets, targets, config)
+        if config.raw_covariates:
+            _logger.info("Saving raw data before any processing")
+            ls.features.save_intersected_features_and_targets(image_chunk_sets,
+                                                              transform_sets, targets, config)
 
-            if config.rank_features:
-                _logger.info("Ranking features...")
-                measures, features, scores = \
-                    ls.validate.local_rank_features(image_chunk_sets, transform_sets, targets, config)
-                ls.mpiops.run_once(ls.geoio.export_feature_ranks, measures, features, scores, config)
+        if config.rank_features:
+            _logger.info("Ranking features...")
+            measures, features, scores = \
+                ls.validate.local_rank_features(image_chunk_sets, transform_sets, targets, config)
+            ls.mpiops.run_once(ls.geoio.export_feature_ranks, measures, features, scores, config)
 
-            # need to add cubist cols to config.algorithm_args
-            # keep: bool array corresponding to rows that are retained
-            features, keep = ls.features.transform_features(image_chunk_sets,
-                                                            transform_sets,
-                                                            config.final_transform,
-                                                            config)
-            # learn the model
-            # local models need all data
-            x_all = ls.features.gather_features(features[keep], node=0)
+        # need to add cubist cols to config.algorithm_args
+        # keep: bool array corresponding to rows that are retained
+        features, keep = ls.features.transform_features(image_chunk_sets,
+                                                        transform_sets,
+                                                        config.final_transform,
+                                                        config)
+        # learn the model
+        # local models need all data
+        x_all = ls.features.gather_features(features[keep], node=0)
 
-            # We're doing local models at the moment
-            targets_all = ls.targets.gather_targets(targets, keep, config, node=0)
+        # We're doing local models at the moment
+        targets_all = ls.targets.gather_targets(targets, keep, config, node=0)
 
-            # Pickle data if requested.
-            if ls.mpiops.chunk_index == 0:
-                if config.pk_covariates and not os.path.exists(config.pk_covariates):
-                    pickle.dump(x_all, open(config.pk_covariates, 'wb'))
-                if config.pk_targets and not os.path.exists(config.pk_targets):
-                    pickle.dump(targets_all, open(config.pk_targets, 'wb'))
+        # Pickle data if requested.
+        if ls.mpiops.chunk_index == 0:
+            if config.pk_covariates and not os.path.exists(config.pk_covariates):
+                pickle.dump(x_all, open(config.pk_covariates, 'wb'))
+            if config.pk_targets and not os.path.exists(config.pk_targets):
+                pickle.dump(targets_all, open(config.pk_targets, 'wb'))
 
-        return targets_all, x_all
+    return targets_all, x_all
 
 @cli.command()
 @click.argument('config_file')
