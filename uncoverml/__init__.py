aacc684ce1445fe18f32729749176164435cac98
Bren Moushall <bmoush@gmail.com>
2020-01-21 23:49:48 +0000

    Disable MPI network interface warning by default

--- uncoverml/__init__.py
+++ uncoverml/__init__.py
@@ -11,3 +11,6 @@ __version__ = pkg_resources.get_distribution('uncover-ml').version
 if 'UNCOVERML_SRC' not in os.environ:
     os.environ['UNCOVERML_SRC'] = os.path.split(uncoverml.__path__[0])[0]
 
+# Turn off MPI warning about network interface
+os.environ['OMPI_MCA_btl_base_warn_component_unused'] = '0'
+
