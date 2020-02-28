abf1b52086717db2ff737ffe5ffc55d6c0954380
Bren Moushall <bmoush@gmail.com>
2020-02-28 18:48:16 +1100

    Refactor generating dummy targets
    Separate dummy target generation from shiftmap implementation and
    allow generating dummy targets within a given bounds. This is also
    needed for 'target search' functionality where we need to randomly
    sample points in a prediction area.

--- uncoverml/scripts/shiftmap_cli.py
+++ uncoverml/scripts/shiftmap_cli.py
@@ -66,7 +66,10 @@ def main(config_file, partitions):
         real_targets = ls.targets.label_targets(real_targets, 'training')
         targets = ls.targets.merge_targets(dummy_targets, real_targets)
     else:
-        targets = ls.targets.generate_covariate_shift_targets(real_targets, ls.geoio.get_image_bounds(config))
+        bounds = ls.geoio.get_image_bounds(config)
+        bounds = (bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1])
+        targets = ls.targets.generate_covariate_shift_targets(real_targets, bounds, 
+                                                              config.shiftmap_points)
 
     image_chunk_sets = ls.geoio.image_feature_sets(targets, config)
     transform_sets = [k.transform_set for k in config.feature_sets]
@@ -76,7 +79,6 @@ def main(config_file, partitions):
                                                     config)
     x_all = ls.features.gather_features(features[keep], node=0)
     targets_all = ls.targets.gather_targets(targets, keep, config, node=0)
-    ls.targets.save_dummy_targets(targets_all, config)
     model = ls.models.LogisticClassifier(random_state=1)
     ls.models.apply_multiple_masked(model.fit, (x_all, targets_all.observations),
                                     kwargs={'fields': targets_all.fields,
