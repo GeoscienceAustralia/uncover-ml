3fc01a7a7a66b2da276c19f7e67d2c03640806ef
Bren Moushall <bmoush@gmail.com>
2020-03-18 10:52:26 +1100

    Fix config tests to work with new parsing

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,10 +13,10 @@ from uncoverml import transforms
 def config_object(sirsam_rf_conf, sirsam_rf_out):
     if os.path.exists(sirsam_rf_out):
         shutil.rmtree(sirsam_rf_out)
-    return Config(sirsam_rf_conf)
+    return Config(sirsam_rf_conf, learning=True)
 
-def test_attributes_exist(config_object, sirsam_rf_conf, sirsam_rf_out, 
-                          sirsam_covariate_paths, sirsam_target_path):
+def test_parse_learning_config(config_object, sirsam_rf_conf, sirsam_rf_out, 
+                               sirsam_covariate_paths, sirsam_target_path):
     c = config_object
     assert c.patchsize == 0
     assert c.algorithm == 'multirandomforest'
@@ -27,15 +27,10 @@ def test_attributes_exist(config_object, sirsam_rf_conf, sirsam_rf_out,
     assert c.algorithm_args['n_estimators'] == 10
     assert c.algorithm_args['target_transform'] == 'log'
     assert c.algorithm_args['forests'] == 20
-    assert c.quantiles == 0.95
-    assert c.geotif_options == {}
-    assert c.outbands == 10
-    assert c.thumbnails == 10
     assert c.raw_covariates == os.path.join(sirsam_rf_out, c.name + '_rawcovariates.csv')
     assert c.pk_covariates == os.path.join(sirsam_rf_out, 'features.pk')
     assert c.pk_targets == os.path.join(sirsam_rf_out, 'targets.pk')
     assert c.pk_load == False
-    assert not hasattr(c, 'featurevec')
     assert len(c.feature_sets) == 1
     assert c.feature_sets[0].type == 'ordinal'
     assert sorted(c.feature_sets[0].files) == sorted(sirsam_covariate_paths)
@@ -47,13 +42,6 @@ def test_attributes_exist(config_object, sirsam_rf_conf, sirsam_rf_out,
                for t in ts.global_transforms)
     assert isinstance(ts.imputer, transforms.MeanImputer)
     assert c.final_transform == None
-    assert c.target_file == sirsam_target_path
-    assert c.target_property == 'Na_log'
-    assert c.mask == None
-    assert not hasattr(c, 'retain')
-    assert c.lon_lat == None
-    assert not hasattr(c, 'lon')
-    assert not hasattr(c, 'lat')
     assert c.rank_features == True
     assert c.permutation_importance == False
     assert c.cross_validate == True
@@ -64,9 +52,3 @@ def test_attributes_exist(config_object, sirsam_rf_conf, sirsam_rf_out,
     assert c.optimisation == None
     assert c.clustering == False
     assert c.cluster_analysis == False
-    assert not hasattr(c, 'clustering_algorithm')
-    assert not hasattr(c, 'n_classes')
-    assert not hasattr(c, 'oversample_factor')
-    assert not hasattr(c, 'semi_supervised')
-    assert not hasattr(c, 'class_file')
-    assert not hasattr(c, 'class_property')
