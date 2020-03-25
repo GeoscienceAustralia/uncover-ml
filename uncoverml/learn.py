6fc0a9de09ae2cf9634f2134119dcd56cd3f15b4
Bren Moushall <bmoush@gmail.com>
2020-03-25 14:04:18 +1100

    Refactor bootstrap to models module
    Bootstrapping is now done internally within a Model container class.
    This allows the config to be simplified by activating bootstrapping by
    using a 'bootstrap*' class (e.g. 'bootstrapsvr') and providing
    bootstrapping arguments as part of the algorithm args block.
    
    It also simplifies the code by not requiring multiple checks for
    'config.bootstrapping' that lead to duplicated learning code that
    accomodates bootstrapping.
    
    Prediction still needs to be tested.
    
    
    Former-commit-id: 8fa76c6b829e0ffc74deb7ac70e33a180f95e45b

--- uncoverml/learn.py
+++ uncoverml/learn.py
@@ -16,108 +16,14 @@ from uncoverml.optimise.models import transformed_modelmaps
 _logger = logging.getLogger(__name__)
 all_modelmaps = {**transformed_modelmaps, **modelmaps, **krig_dict}
 
-# Should we put this in a model class?
-# We could try and create a BootstrapEnsemble container class that 
-#  takes a model as an init argument.
-# Similar to multirandomforest but more generic.
-def bootstrap_model(x_all, targets_all, config):
-    """
-    Use bootstrap resampling on target data to generate an ensemble of
-    models, allowing for probabilistic predictions.
-
-    Parameters
-    ----------
-    x_all
-        Array of covariate data, containing data at target locations.
-    targets_all : Targets
-        Collection of training targets.
-    n : int
-        Number of models to train (each model is trained on 
-        separately resampled training data).
-
-    Returns
-    -------
-    list of models
-        A list of uncoverml `Model`s, each trained on bootstrapped
-        training data.
-    """
-    models = []
-
-    if mpiops.chunk_index == 0:
-        obs = targets_all.observations
-        pos = targets_all.positions
-    else:
-        obs = None
-        pos = None
-
-    shared_x, x_win = mpiops.create_shared_array(x_all)
-    shared_t, t_win = mpiops.create_shared_array(obs) 
-    shared_p, p_win = mpiops.create_shared_array(pos)
-
-    if config.bootstrap_pickle is not None \
-            and os.path.exists(os.path.abspath(config.bootstrap_pickle)):
-        if mpiops.chunk_index == 0:
-            _logger.info("Loading bootstrapped data views from file...")
-            with open(os.path.abspath(config.bootstrap_pickle), 'rb') as f:
-                inds = pickle.load(f)
-        else:
-            inds = None
-        inds = mpiops.comm.bcast(inds, root=0)
-        _logger.info(f"Loaded {len(inds)} data views")
-        inds = np.array_split(inds, mpiops.chunks)[mpiops.chunk_index]
-    else:
-       _logger.info("Bootstrapping data %s times", config.bootstrap_models)
-       iterations = len(np.array_split(range(config.bootstrap_models), mpiops.chunks)[mpiops.chunk_index])
-       inds = []
-       for i in range(iterations):
-           inds.append(resampling.bootstrap_data_indicies(shared_p, max(shared_p.shape), 
-               random_state=mpiops.chunk_index + 1 + i))
-           _logger.info(f":mpi:bootstrapped {i + 1} of {iterations}")
-
-       if config.bootstrap_pickle:
-           all_inds = mpiops.comm.gather(inds, root=0)
-           if mpiops.chunk_index == 0:
-               all_inds = list(itertools.chain.from_iterable(all_inds))
-               with open(os.path.abspath(config.bootstrap_pickle), 'wb') as f:
-                   pickle.dump(all_inds, f)
-               _logger.info("Pickled bootstrapped data views...")
-    
-       _logger.info("Bootstrapping complete, training models...")
-
-    mpiops.comm.barrier()
-
-    for i, ind in enumerate(inds):
-        bootstrapped_x = shared_x[ind]
-        bootstrapped_t = shared_t[ind]
-        model = all_modelmaps[config.algorithm](**config.algorithm_args)
-        apply_multiple_masked(model.fit, (bootstrapped_x, bootstrapped_t))
-        models.append(model)
-        _logger.info(f":mpi:Trained model {i + 1} of {len(inds)}")
-
-    mpiops.comm.barrier()
-    _logger.info(f"Complete! {config.bootstrap_models} trained")
-    shared_x = None
-    x_win.Free()
-    shared_t = None
-    t_win.Free()
-
-    # Gather models onto master
-    models = mpiops.comm.gather(models, root=0)
-    if mpiops.chunk_index == 0:
-        models = list(itertools.chain.from_iterable(models))
-    return models
-
-
 def local_learn_model(x_all, targets_all, config):
     mpiops.comm.barrier()
     model = None
-    if config.multicubist or config.multirandomforest:
+    if config.multicubist or config.multirandomforest or config.bootstrap:
         y = targets_all.observations
         model = all_modelmaps[config.algorithm](**config.algorithm_args)
-        apply_multiple_masked(model.fit, (x_all, y),
-                              kwargs={'fields': targets_all.fields,
-                                      'parallel': True,
-                                      'lon_lat': targets_all.positions})
+        apply_multiple_masked(model.fit, (x_all, y), fields=targets_all.fields, parallel=True, 
+                              lon_lat=targets_all.positions)
         if config.multirandomforest:
             rf_dicts = model._randomforests
             rf_dicts = mpiops.comm.gather(rf_dicts, root=0)
@@ -129,9 +35,8 @@ def local_learn_model(x_all, targets_all, config):
         if mpiops.chunk_index == 0:
             y = targets_all.observations
             model = all_modelmaps[config.algorithm](**config.algorithm_args)
-            apply_multiple_masked(model.fit, (x_all, y),
-                                  kwargs={'fields': targets_all.fields,
-                                          'lon_lat': targets_all.positions})
+            apply_multiple_masked(model.fit, (x_all, y), 
+                                  fields=targets_all.fields, lon_lat=targets_all.positions)
 
     # Save transformed targets for diagnostics
     if mpiops.chunk_index == 0 and hasattr(model, 'target_transform'):
