f2276a4b8bb5323f1b00a32ae249459b96845557
sheecegardezi <sheecegardezi@gmail.com>
2021-01-14 11:54:08 +1100

    inital commit

---
+++ jupyter/jupyter_install.sh
@@ -0,0 +1,27 @@
+module purge
+module load pbs
+module load python3/3.7.4
+module load gdal/3.0.2
+
+cd $HOME
+
+if [ ! -d $HOME/uncover-ml ]
+then
+     git clone --single-branch --branch sheece-jupyter https://github.com/GeoscienceAustralia/uncover-ml.git
+     cd uncover-ml/jupyter
+else
+     cd uncover-ml
+     git pull
+     git checkout sheece-jupyter
+     cd jupyter
+fi
+
+mkdir -p $HOME/venvs/jupyter
+python3 -m venv $HOME/venvs/jupyter
+source $HOME/venvs/jupyter/bin/activate
+
+
+pip install -r requirements.txt
+chmod +x jupyter.node.sh 
+chmod +x jupyter.host.sh 
+qsub jupyter.host.sh 
