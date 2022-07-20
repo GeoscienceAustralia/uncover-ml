0bd736d09a1b6d3b02e22a91735d962f3ecd527d
u25867 <aditya.sevak@ga.gov.au>
2022-07-20 11:27:46 +1000

    testing-shap

--- uncoverml/scripts/uncoverml.py
+++ uncoverml/scripts/uncoverml.py
@@ -94,6 +94,7 @@ def learn(pipeline_file, param_json, partitions):
         ls.mpiops.run_once(ls.validate.permutation_importance, model, x_all,
                            targets_all, config)
 
+    log.info('Calculating shap values')
     ls.validate.calc_shap(x_all, model, config)
 
     # if config.feature_importance:
