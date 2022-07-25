754bc97a4c175b7dbf26249081daf81e1f051c85
u25867 <aditya.sevak@ga.gov.au>
2022-07-25 14:36:49 +1000

    testing shap

--- uncoverml/scripts/uncoverml.py
+++ uncoverml/scripts/uncoverml.py
@@ -394,6 +394,7 @@ def predict(model_or_cluster_file, partitions, mask, retain):
     # ls.predict.final_cluster_analysis(config.n_classes,
     #                                   config.n_subchunks)
 
+    log.info('Calculating shap values')
     targets_all, x_all = _load_data(config, partitions)
     ls.validate.calc_shap(x_all, model, config)
 
