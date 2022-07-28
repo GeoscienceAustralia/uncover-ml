950c48192199e719c4c36c1d5b61f3e296a95e28
u25867 <aditya.sevak@ga.gov.au>
2022-07-28 11:48:45 +1000

    testing shap

--- shap_test.py
+++ shap_test.py
@@ -38,7 +38,6 @@ if __name__ == '__main__':
 
     # Aggregate:
     #   - Benchmark, used to benchmark different explainers
-    #   - Decision
     #   - Embedding
     #   - Group Difference
     #   - Monitoring
@@ -51,14 +50,16 @@ if __name__ == '__main__':
 
 # ----------------------------- Aggregate Plots -----------------------------------------------
 
-    # for idx in range(shap_vals.shape[2]):
-    #     shap.plots.beeswarm(shap_vals[:, :, idx], show=False)
-    #     filename = 'gbquantile/beeswarm_test_' + str(idx) + '.png'
-    #     plt.savefig(filename)
-    #     plt.clf()
-    #
-    # print('beeswarm complete')
+    # BEESWARM - WORKS
+    for idx in range(shap_vals.shape[2]):
+        shap.summary_plot(shap_vals[:, :, idx], show=False)
+        filename = 'gbquantile/beeswarm_test_' + str(idx) + '.png'
+        plt.savefig(filename)
+        plt.clf()
 
+    print('beeswarm complete')
+
+    # BAR - WORKS
     # for idx in range(shap_vals.shape[2]):
     #     shap.plots.bar(shap_vals[:, :, idx], show=False)
     #     filename = 'gbquantile/bar_test_' + str(idx) + '.png'
@@ -67,13 +68,17 @@ if __name__ == '__main__':
     #
     # print('bar complete')
 
-    for idx in range(shap_vals.shape[2]):
-        shap.plots.decision(shap_vals[:, :, idx].base_values[0], shap_vals[:, :, idx].values, show=False)
-        filename = 'gbquantile/dec_test_' + str(idx) + '.png'
-        plt.savefig(filename)
-        plt.clf()
+    # DECISION - WORKS
+    # for idx in range(shap_vals.shape[2]):
+    #     shap.plots.decision(shap_vals[:, :, idx].base_values[0], shap_vals[:, :, idx].values, show=False)
+    #     filename = 'gbquantile/dec_test_' + str(idx) + '.png'
+    #     plt.savefig(filename)
+    #     plt.clf()
+    #
+    # print('decision plot complete')
 
-    print('decision plot complete')
+    # EMBEDDING PLOT
+
 
 
 # ----------------------------- Individual Plots -----------------------------------------------
