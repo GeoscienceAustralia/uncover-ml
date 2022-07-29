93910f716118783d5a465b423835a564f44b151a
u25867 <aditya.sevak@ga.gov.au>
2022-07-29 15:39:38 +1000

    testing shap

--- shap_test.py
+++ shap_test.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     masker = shap.maskers.Independent(x_all)
     explainer = shap.Explainer(predict_for_shap, masker)
     print('calculating shap values')
-    shap_vals = explainer(x_all[:1500])
+    shap_vals = explainer(x_all[:10])
 
     print('plotting shap values')
 
