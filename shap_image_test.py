a8ece1d3093d37182bbd1fd99960e00be6166f79
u25867 <aditya.sevak@ga.gov.au>
2022-08-02 12:09:51 +1000

    testing shap

--- shap_image_test.py
+++ shap_image_test.py
@@ -25,13 +25,14 @@ from uncoverml import predict
 log = logging.getLogger(__name__)
 
 
-def shap_image(model_or_cluster_file, partitions, mask, retain):
+def predict_save(model_or_cluster_file, partitions, mask, retain):
     with open(model_or_cluster_file, 'rb') as f:
         state_dict = joblib.load(f)
 
     model = state_dict["model"]
     config = state_dict["config"]
-
+    config.cluster = True if os.path.splitext(model_or_cluster_file)[1] == '.cluster' \
+        else False
     config.mask = mask if mask else config.mask
     if config.mask:
         config.retain = retain if retain else config.retain
@@ -63,38 +64,27 @@ def shap_image(model_or_cluster_file, partitions, mask, retain):
                                                       config.outbands)],
                                      **config.geotif_options)
 
-    render_partition_shap(model, 0, image_out, config)
+    for i in range(config.n_subchunks):
+        log.info("starting to render partition {}".format(i+1))
+        ls.predict.render_partition(model, i, image_out, config)
+
+    # explicitly close output rasters
     image_out.close()
+
+    if config.cluster and config.cluster_analysis:
+        if ls.mpiops.chunk_index == 0:
+            ls.predict.final_cluster_analysis(config.n_classes,
+                                              config.n_subchunks)
+
+    # ls.predict.final_cluster_analysis(config.n_classes,
+    #                                   config.n_subchunks)
 
     if config.thumbnails:
         image_out.output_thumbnails(config.thumbnails)
-    log.info("Finished! Total mem = {:.1f} GB".format(_total_gb()))
-
-
-def render_partition_shap(model, subchunk, image_out: geoio.ImageWriter, config: Config):
-    def predict_for_shap(x_vals):
-        # noinspection PyProtectedMember
-        predictions = predict.predict(x_vals, model, interval=config.quantiles,
-                                      lon_lat=predict._get_lon_lat(subchunk, config))
-        return predictions
-
-    # noinspection PyProtectedMember
-    x, feature_names = predict._get_data(subchunk, config)
-    total_gb = mpiops.comm.allreduce(x.nbytes / 1e9)
-    log.info("Loaded {:2.4f}GB of image data".format(total_gb))
-    alg = config.algorithm
-    log.info("Predicting targets for {}.".format(alg))
-    masker = shap.maskers.Independent(x)
-    explainer = shap.Explainer(predict_for_shap, masker)
-    shap_vals = explainer(x)
-    shap_vals_to_write = shap_vals[:, 0, 0]
-
-    image_out.write(shap_vals_to_write, subchunk)
+    log.info("Finished!")
 
 
 if __name__ == '__main__':
     model_file = 'gbquantile/gbquantiles.model'
     partitions = 200
-    shap_image(model_file, partitions, None, None)
-    print('shap image calc complete')
-
+    predict_save(model_file, partitions, None, None)
