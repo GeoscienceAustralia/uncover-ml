28fd78676b47035461aa9cea0f2e8aceb7a63b11
u25867 <aditya.sevak@ga.gov.au>
2022-08-30 12:28:41 +1000

    fixing plot bugs

--- uncoverml/shapley.py
+++ uncoverml/shapley.py
@@ -419,7 +419,8 @@ def waterfall_plot(plot_data, plot_config, target_ax, plot_idx, **kwargs):
 
 def summary_plot(plot_data, plot_config, target_ax, plot_idx, **kwargs):
     plt.sca(target_ax)
-    shap.summary_plot(plot_data.values, features=plot_data.data, feature_names=plot_data.feature_names, show=False)
+    shap.summary_plot(plot_data.values, features=plot_data.data, feature_names=plot_data.feature_names, show=False,
+                      plot_size=(16, 11))
 
     # x_axis = target_ax.axes.get_xaxis()
     # x_label = x_axis.get_label()
