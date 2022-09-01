bc37d4e6104609733a9c7e44f92f8c211c406586
u25867 <aditya.sevak@ga.gov.au>
2022-09-02 09:52:57 +1000

    rolling back to old way

--- uncoverml/shapley.py
+++ uncoverml/shapley.py
@@ -58,7 +58,7 @@ Properties for shap config
 
 
 def intersect_shp(single_row_df, image_source_dir, **kwargs):
-    geoms = single_row_df.geometry[0]
+    geoms = single_row_df.geometry.values
     geoms = [mapping(geoms)]
     # extract the raster values within the polygon
     with rasterio.open(image_source_dir) as src:
