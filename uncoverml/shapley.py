5496b92ec69e289481e39dd54c440d37123a4b62
u25867 <aditya.sevak@ga.gov.au>
2022-09-05 09:59:08 +1000

    Bug fixed for shap point plots
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs
    
    fixing point plot bugs

--- uncoverml/shapley.py
+++ uncoverml/shapley.py
@@ -15,6 +15,7 @@ from shapely.ops import transform
 from functools import partial
 from rasterio import Affine
 import pyproj
+from textwrap import wrap
 
 from multiprocessing import Pool
 
@@ -151,6 +152,7 @@ def load_point_poly_data(shap_config, main_config):
 
     out_result = {}
     for name in name_list:
+        print(f'Getting data for {name}')
         current_row = loaded_shapefile[loaded_shapefile['Name'] == name]
         current_poly_data = gen_poly_data(current_row, shap_config, main_config)
         out_result[name] = current_poly_data
@@ -178,7 +180,7 @@ def gen_poly_from_point(single_row_df, main_config, size):
             name = path.abspath(tif)
             x = intersect_point_neighbourhood(single_row_df, size, name)
             val_count = x.size
-            print(f'{tif}: {val_count}')
+            # print(f'{tif}: {val_count}')
             x = np.reshape(x, (val_count, 1, 1, 1))
             x = ma.array(x, mask=np.zeros([val_count, 1, 1, 1]))
             # TODO this may hurt performance. Consider removal
@@ -358,13 +360,17 @@ def calc_shap_vals(model, shap_config, x_data, num_proc=1):
     else:
         explainer_obj = explainer_map[shap_config.explainer]['function'](shap_predict, *reqs)
 
-    if x_data.shape[0] > 1:
-        calc_start_row = shap_config.calc_start_row if shap_config.calc_start_row is not None else 0
-        calc_end_row = shap_config.calc_end_row if shap_config.calc_end_row is not None else -1
-        calc_data = x_data[calc_start_row:calc_end_row]
-    else:
-        calc_data = np.reshape(x_data, (x_data.size, 1))
+    calc_start_row = shap_config.calc_start_row if shap_config.calc_start_row is not None else 0
+    calc_end_row = shap_config.calc_end_row if shap_config.calc_end_row is not None else -1
+    calc_data = x_data[calc_start_row:calc_end_row]
 
+    if shap_config.calc_start_row is not None:
+        x_data = x_data[shap_config.calc_start_row:]
+
+    if shap_config.calc_end_row is not None:
+        x_data = x_data[:shap_config.calc_end_row]
+
+    calc_data = x_data
     shap_vals = explainer_obj(calc_data)
     return shap_vals
 
@@ -701,9 +707,10 @@ def generate_plots_poly_point(name_list, shap_vals_dict, shap_vals_point, shap_c
     if kwargs['feature_names'] is None:
         log.warning('Feature names not provided, plots might be confusing')
     else:
-        shap_vals_point.feature_names = kwargs['feature_names']
+        feature_names = ['\n'.join(wrap(feat, 30)) for feat in kwargs['feature_names']]
+        shap_vals_point.feature_names = feature_names
         for key, val in shap_vals_dict.items():
-            val.feature_names = kwargs['feature_names']
+            val.feature_names = feature_names
 
     for idx, name in enumerate(name_list):
         print(f'Generating plot {idx+1} of {len(name_list)}')
@@ -713,12 +720,24 @@ def generate_plots_poly_point(name_list, shap_vals_dict, shap_vals_point, shap_c
         point_poly_subplots(name, current_point_poly_vals, current_point_vals, shap_config, **kwargs)
 
 
+def ax_tidy_point_poly(target_ax, plot_title, padding=None):
+    current_plot_title = '\n'.join(wrap(plot_title, 30))
+    target_ax.set_title(current_plot_title, fontsize=7)
+    target_ax.tick_params(axis='both', labelsize=3)
+    target_ax.set_yticklabels(axs[0, 0].get_yticklabels(), rotation=45)
+    target_ax.xaxis.get_label().set_fontsize(7)
+    if padding is not None:
+        target_ax.tick_params(axis='y', pad=padding)
+
+
 def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs):
-    num_plots = point_vals.shape[2] if len(point_vals.shape) > 2 else 1
+    num_plots = point_poly_vals.shape[2] if len(point_poly_vals.shape) > 2 else 1
     output_names = kwargs['output_names'] if 'output_names' in kwargs else None
 
-    fig, axs = plt.subplots(2, 3, figsize=(1.920, 1.080), dpi=100)
+    plot_width = 66
+    plot_height = 66
     for plot_idx in range(num_plots):
+        fig, axs = plt.subplots(2, 3, figsize=(plot_width, plot_height), dpi=250)
         current_output_name = output_names[plot_idx] if output_names is not None else plot_idx
         current_points_vals = point_vals[:, plot_idx]
         current_point_poly_vals = point_poly_vals[:, :, plot_idx]
@@ -726,49 +745,49 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         # Single prediction waterfall
         plt.sca(axs[0, 0])
         shap.waterfall_plot(current_points_vals, show=False)
-        current_plot_title = f'Single Prediction Waterfall {name} Output {current_output_name}'
-        axs[0, 0].title.set_text(current_plot_title)
+        current_plot_title = f'Single Prediction Waterfall'
+        ax_tidy_point_poly(axs[0, 0], current_plot_title, 10)
 
         # Multi prediction summary
         plt.sca(axs[1, 0])
         shap.summary_plot(current_point_poly_vals.values, features=current_point_poly_vals.data,
-                          feature_names=current_point_poly_vals.feature_names, show=False)
-        current_plot_title = f'Multi-Prediction Summary {name} Output {current_output_name}'
-        axs[1, 0].title.set_text(current_plot_title)
+                          feature_names=current_point_poly_vals.feature_names, show=False,
+                          plot_size=(plot_width, plot_height), color_bar=False)
+        current_plot_title = f'Multi-Prediction Summary'
+        ax_tidy_point_poly(axs[1, 0], current_plot_title)
 
         # Single prediction bar
         plt.sca(axs[0, 1])
         shap.plots.bar(current_points_vals, show=False)
-        current_plot_title = f'Single Prediction Bar {name} Output {current_output_name}'
-        axs[0, 1].title.set_text(current_plot_title)
+        current_plot_title = f'Single Prediction Bar'
+        ax_tidy_point_poly(axs[0, 1], current_plot_title, 10)
 
         # Multi prediction bar
         plt.sca(axs[1, 1])
         shap.plots.bar(current_point_poly_vals, show=False)
-        current_plot_title = f'Multi-Prediction Bar {name} Output {current_output_name}'
-        axs[1, 1].title.set_text(current_plot_title)
+        current_plot_title = f'Multi-Prediction Bar'
+        axs[1, 1].xaxis.get_label().set_fontsize(7)
+        ax_tidy_point_poly(axs[1, 1], current_plot_title, 10)
 
         # Single prediction decision
         plt.sca(axs[0, 2])
         shap.decision_plot(current_points_vals.base_values, current_points_vals.values,
-                           feature_names=current_points_vals.feature_names)
-        current_plot_title = f'Single Prediction Decision {name} Output {current_output_name}'
-        axs[0, 2].title.set_text(current_plot_title)
+                           feature_names=current_points_vals.feature_names, auto_size_plot=False)
+        current_plot_title = f'Single Prediction Decision'
+        ax_tidy_point_poly(axs[0, 2], current_plot_title)
 
         # Multi prediction decision
         plt.sca(axs[1, 2])
         shap.decision_plot(current_point_poly_vals.base_values[0], current_point_poly_vals.values,
-                           feature_names=current_point_poly_vals.feature_names)
-        current_plot_title = f'Multi-Prediction Decision {name} Output {current_output_name}'
-        axs[0, 2].title.set_text(current_plot_title)
+                           feature_names=current_point_poly_vals.feature_names, auto_size_plot=False)
+        current_plot_title = f'Multi-Prediction Decision'
+        ax_tidy_point_poly(axs[1, 2], current_plot_title)
 
         fig.suptitle(f'Single vs Multiple Point {name} Output {current_output_name}')
 
         plot_name = f'poly_point_{name}_{current_output_name}'
         Path(shap_config.output_path).mkdir(parents=True, exist_ok=True)
         plot_save_path = path.join(shap_config.output_path, plot_name + '.png')
-        plot_width = 16.5
-        plot_height = 11.75
-        fig.set_size_inches(plot_width, plot_height, forward=True)
-        plt.tight_layout(pad=3)
-        fig.savefig(plot_save_path, dpi=100)
+        fig.tight_layout()
+        fig.savefig(plot_save_path, dpi=250)
+        plt.clf()
