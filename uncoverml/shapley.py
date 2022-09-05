ee209b48a7b29dbbbd277ceddaa95d9a481f7876
u25867 <aditya.sevak@ga.gov.au>
2022-09-05 14:27:16 +1000

    fixing point plot bugs

--- uncoverml/shapley.py
+++ uncoverml/shapley.py
@@ -736,7 +736,7 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         plt.sca(axs[0, 0])
         shap.waterfall_plot(current_points_vals, show=False)
         current_plot_title = f'Single Prediction Waterfall {name} Output {current_output_name}'
-        current_plot_title = '\n'.join(wrap(current_plot_title, 20))
+        current_plot_title = '\n'.join(wrap(current_plot_title, 30))
         axs[0, 0].set_title(current_plot_title, fontsize=7)
         axs[0, 0].tick_params(axis='both', labelsize=3)
         axs[0, 0].tick_params(axis='y', pad=15)
@@ -749,7 +749,7 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
                           feature_names=current_point_poly_vals.feature_names, show=False,
                           plot_size=(plot_width, plot_height), color_bar=False)
         current_plot_title = f'Multi-Prediction Summary {name} Output {current_output_name}'
-        current_plot_title = '\n'.join(wrap(current_plot_title, 20))
+        current_plot_title = '\n'.join(wrap(current_plot_title, 30))
         axs[1, 0].set_title(current_plot_title, fontsize=7)
         axs[1, 0].tick_params(axis='both', labelsize=3)
         axs[1, 0].set_yticklabels(axs[1, 0].get_yticklabels(), rotation=45)
@@ -759,7 +759,7 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         plt.sca(axs[0, 1])
         shap.plots.bar(current_points_vals, show=False)
         current_plot_title = f'Single Prediction Bar {name} Output {current_output_name}'
-        current_plot_title = '\n'.join(wrap(current_plot_title, 20))
+        current_plot_title = '\n'.join(wrap(current_plot_title, 30))
         axs[0, 1].set_title(current_plot_title, fontsize=7)
         axs[0, 1].tick_params(axis='both', labelsize=3)
         axs[0, 1].tick_params(axis='y', pad=15)
@@ -770,7 +770,7 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         plt.sca(axs[1, 1])
         shap.plots.bar(current_point_poly_vals, show=False)
         current_plot_title = f'Multi-Prediction Bar {name} Output {current_output_name}'
-        current_plot_title = '\n'.join(wrap(current_plot_title, 20))
+        current_plot_title = '\n'.join(wrap(current_plot_title, 30))
         axs[1, 1].set_title(current_plot_title, fontsize=7)
         axs[1, 1].tick_params(axis='both', labelsize=3)
         axs[1, 1].tick_params(axis='y', pad=15)
@@ -782,7 +782,7 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         shap.decision_plot(current_points_vals.base_values, current_points_vals.values,
                            feature_names=current_points_vals.feature_names, auto_size_plot=False)
         current_plot_title = f'Single Prediction Decision {name} Output {current_output_name}'
-        current_plot_title = '\n'.join(wrap(current_plot_title, 20))
+        current_plot_title = '\n'.join(wrap(current_plot_title, 30))
         axs[0, 2].set_title(current_plot_title, fontsize=7)
         axs[0, 2].tick_params(axis='both', labelsize=3)
         axs[0, 2].set_yticklabels(axs[0, 2].get_yticklabels(), rotation=45)
@@ -793,7 +793,7 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         shap.decision_plot(current_point_poly_vals.base_values[0], current_point_poly_vals.values,
                            feature_names=current_point_poly_vals.feature_names, auto_size_plot=False)
         current_plot_title = f'Multi-Prediction Decision {name} Output {current_output_name}'
-        current_plot_title = '\n'.join(wrap(current_plot_title, 20))
+        current_plot_title = '\n'.join(wrap(current_plot_title, 30))
         axs[1, 2].set_title(current_plot_title, fontsize=7)
         axs[1, 2].tick_params(axis='both', labelsize=3)
         axs[1, 2].set_yticklabels(axs[1, 2].get_yticklabels(), rotation=45)
