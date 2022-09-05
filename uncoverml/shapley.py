063e0306d12be75669c629f853bde1ce0e9a44f8
u25867 <aditya.sevak@ga.gov.au>
2022-09-05 13:00:40 +1000

    fixing point plot bugs

--- uncoverml/shapley.py
+++ uncoverml/shapley.py
@@ -735,9 +735,9 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         plt.sca(axs[0, 0])
         shap.waterfall_plot(current_points_vals, show=False)
         current_plot_title = f'Single Prediction Waterfall {name} Output {current_output_name}'
-        current_plot_title = wrap(current_plot_title, 20)
+        current_plot_title = '\n'.join(wrap(current_plot_title, 20)
         axs[0, 0].set_title(current_plot_title, fontsize=7)
-        axs[0, 0].tick_params(axis='both', labelsize=7)
+        axs[0, 0].tick_params(axis='both', labelsize=5)
         axs[0, 0].set_yticklabels(axs[0, 0].get_yticklabels(), rotation=45)
         axs[0, 0].xaxis.get_label().set_fontsize(7)
 
@@ -747,9 +747,9 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
                           feature_names=current_point_poly_vals.feature_names, show=False,
                           plot_size=(plot_width, plot_height))
         current_plot_title = f'Multi-Prediction Summary {name} Output {current_output_name}'
-        current_plot_title = wrap(current_plot_title, 20)
+        current_plot_title = '\n'.join(wrap(current_plot_title, 20)
         axs[1, 0].set_title(current_plot_title, fontsize=7)
-        axs[1, 0].tick_params(axis='both', labelsize=7)
+        axs[1, 0].tick_params(axis='both', labelsize=5)
         axs[1, 0].set_yticklabels(axs[1, 0].get_yticklabels(), rotation=45)
         axs[1, 0].xaxis.get_label().set_fontsize(7)
 
@@ -757,9 +757,9 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         plt.sca(axs[0, 1])
         shap.plots.bar(current_points_vals, show=False)
         current_plot_title = f'Single Prediction Bar {name} Output {current_output_name}'
-        current_plot_title = wrap(current_plot_title, 20)
+        current_plot_title = '\n'.join(wrap(current_plot_title, 20)
         axs[0, 1].set_title(current_plot_title, fontsize=7)
-        axs[0, 1].tick_params(axis='both', labelsize=7)
+        axs[0, 1].tick_params(axis='both', labelsize=5)
         axs[0, 1].set_yticklabels(axs[0, 1].get_yticklabels(), rotation=45)
         axs[0, 1].xaxis.get_label().set_fontsize(7)
 
@@ -767,9 +767,9 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         plt.sca(axs[1, 1])
         shap.plots.bar(current_point_poly_vals, show=False)
         current_plot_title = f'Multi-Prediction Bar {name} Output {current_output_name}'
-        current_plot_title = wrap(current_plot_title, 20)
+        current_plot_title = '\n'.join(wrap(current_plot_title, 20)
         axs[1, 1].set_title(current_plot_title, fontsize=7)
-        axs[1, 1].tick_params(axis='both', labelsize=7)
+        axs[1, 1].tick_params(axis='both', labelsize=5)
         axs[1, 1].set_yticklabels(axs[1, 1].get_yticklabels(), rotation=45)
         axs[1, 1].xaxis.get_label().set_fontsize(7)
 
@@ -778,9 +778,9 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         shap.decision_plot(current_points_vals.base_values, current_points_vals.values,
                            feature_names=current_points_vals.feature_names, auto_size_plot=False)
         current_plot_title = f'Single Prediction Decision {name} Output {current_output_name}'
-        current_plot_title = wrap(current_plot_title, 20)
+        current_plot_title = '\n'.join(wrap(current_plot_title, 20)
         axs[0, 2].set_title(current_plot_title, fontsize=7)
-        axs[0, 2].tick_params(axis='both', labelsize=7)
+        axs[0, 2].tick_params(axis='both', labelsize=5)
         axs[0, 2].set_yticklabels(axs[0, 2].get_yticklabels(), rotation=45)
         axs[0, 2].xaxis.get_label().set_fontsize(7)
 
@@ -789,9 +789,9 @@ def point_poly_subplots(name, point_poly_vals, point_vals, shap_config, **kwargs
         shap.decision_plot(current_point_poly_vals.base_values[0], current_point_poly_vals.values,
                            feature_names=current_point_poly_vals.feature_names, auto_size_plot=False)
         current_plot_title = f'Multi-Prediction Decision {name} Output {current_output_name}'
-        current_plot_title = wrap(current_plot_title, 20)
+        current_plot_title = '\n'.join(wrap(current_plot_title, 20)
         axs[1, 2].set_title(current_plot_title, fontsize=7)
-        axs[1, 2].tick_params(axis='both', labelsize=7)
+        axs[1, 2].tick_params(axis='both', labelsize=5)
         axs[1, 2].set_yticklabels(axs[1, 2].get_yticklabels(), rotation=45)
         axs[1, 2].xaxis.get_label().set_fontsize(7)
 
