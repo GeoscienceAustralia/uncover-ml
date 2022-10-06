1b31c625d6eef8678902c5c4885d860784eb261c
u25867 <aditya.sevak@ga.gov.au>
2022-10-06 13:29:36 +1100

    fixing clustering

--- cluster_test.py
+++ cluster_test.py
@@ -16,11 +16,12 @@ def training_data_plot(model_file, training_data_file):
 
 
 def prediction_data_plot(config):
-    cluster.all_feat_boxplot(config)
+    current_config = config.Config(config)
+    cluster.all_feat_boxplot(current_config)
 
 
 if __name__ == '__main__':
-    config = './cluster_test/yaml'
+    config = './cluster_test.yaml'
     prediction_data_plot(config)
 
     # model_file = './results/test_cluster.cluster'
