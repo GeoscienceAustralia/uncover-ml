e2786eeced2edc391d118053dd255136115083e7
u25867 <aditya.sevak@ga.gov.au>
2022-10-14 14:29:37 +1100

    fixing shap plot

--- uncoverml/shapley.py
+++ uncoverml/shapley.py
@@ -813,10 +813,7 @@ def ax_tidy_point_poly(target_ax, plot_title, padding=None):
     current_plot_title = '\n'.join(wrap(plot_title, 30))
     target_ax.set_title(current_plot_title, fontsize=7)
     target_ax.tick_params(axis='both', labelsize=5)
-    # target_ax.set_yticklabels(target_ax.get_yticklabels(), rotation=45)
-    ticks_loc = target_ax.get_yticks().tolist()
-    target_ax.yaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
-    target_ax.set_yticklabels(ticks_loc, rotation=45)
+    target_ax.set_yticklabels(target_ax.get_yticklabels(), rotation=45)
     target_ax.xaxis.get_label().set_fontsize(7)
     if padding is not None:
         target_ax.tick_params(axis='y', pad=padding)
