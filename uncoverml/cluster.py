a0d6248bf47f04879dd054c3b70bf2e41b9786ce
u25867 <aditya.sevak@ga.gov.au>
2022-10-20 12:15:59 +1100

    fixing cluster plot issues

--- uncoverml/cluster.py
+++ uncoverml/cluster.py
@@ -733,7 +733,7 @@ def box_plot_from_stats(stats_dict, data_type, config, feat_labels=None):
     fig, axs = plt.subplots(num_plots, 1, sharex=True)
     for idx, (feat, stat) in enumerate(stats_dict.items()):
         target_ax = np.ravel(axs)[idx]
-        if hasattr(config, plot_outliers) and config.plot_outliers:
+        if hasattr(config, 'plot_outliers') and config.plot_outliers:
             target_ax.bxp(stat, flierprops={'marker': 'o', 'markersize': 1, 'markerfacecolor': 'black'})
         else:
             target_ax.bxp(stat, showfliers=False)
