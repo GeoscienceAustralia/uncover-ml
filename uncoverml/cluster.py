d8f72a03459f8a033e27f9eb5d10fea2c0fd8d91
u25867 <aditya.sevak@ga.gov.au>
2022-10-20 12:49:12 +1100

    fixing cluster plot issues

--- uncoverml/cluster.py
+++ uncoverml/cluster.py
@@ -804,10 +804,10 @@ def training_data_scatter(training_data, model, config, feat_labels=None, predic
 
         centres_x = centres[:, feat_idx[0]]
         centres_y = centres[:, feat_idx[1]]
-        ax.scatter(centres_x, centres_y, c='black', s=5, alpha=0.5)
+        ax.scatter(centres_x, centres_y, c='black', s=200, alpha=0.5)
         clust_labels = [i for i in range(config.n_classes)]
         for idx, label in enumerate(clust_labels):
-            ax.annotate(label, (centres_x[idx], centres_y[idx]))
+            ax.annotate(label, (centres_x[idx], centres_y[idx]), ha='center', va='center', c='white')
 
         fig.suptitle(f'{x_data_name}_{y_data_name} Clusters')
         plt.xlabel(x_data_name)
