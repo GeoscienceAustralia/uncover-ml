6c0a72cda51ceace8a1e24f4ca2d33c774f9b25c
u25867 <aditya.sevak@ga.gov.au>
2023-01-24 11:18:47 +1100

    added entrypoint to test validation

--- uncoverml/validate.py
+++ uncoverml/validate.py
@@ -619,7 +619,7 @@ def oos_validate(targets_all, x_all, model, config):
 
         real_and_pred = [to_text[0], to_text[1]]
         real_and_pred_cols = tags + ['y_true']
-        real_and_pred = pd.DataFrame(np.concatenate(real_and_pred))
+        real_and_pred = pd.DataFrame(np.concatenate(real_and_pred, axis=1))
         real_and_pred.columns = real_and_pred_cols
         target_col = 'Prediction' if 'Prediction' in tags else tags[0]
         density_scatter = sns.kdeplot(data=real_and_pred, x=target_col, y='y_true', hue='kind', fill=True)
