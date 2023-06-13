c638b364d546cb150b33a5e08a1d84e199f48073
Sudipta Basak <basaks@gmail.com>
2023-06-13 11:59:09 +1000

    bug fixes on resampling script for validation shape file

--- uncoverml/resampling.py
+++ uncoverml/resampling.py
@@ -65,7 +65,7 @@ def resample_by_magnitude(input_data, target_field, bins=10, interval='linear', 
     input_gdf : geopandas.GeoDataFrame
         Geopandas dataframe containing targets to be resampled.
     target_field : str
-        target field name based on which resampling is performed. Field 
+        target field name based on which resampling is performed. Field
         must exist in the input_shapefile
     bins : int
         number of bins for sampling
@@ -89,13 +89,19 @@ def resample_by_magnitude(input_data, target_field, bins=10, interval='linear', 
     if bootstrap and validation:
         raise ValueError('bootstrapping should not be use while'
                          'creating a validation shapefile.')
-
     if interval not in ['percentile', 'linear']:
         log.warning("Interval method '{}' not recognised, defaulting to 'percentile'"
                     .format(interval))
         interval = 'linear'
     gdf_out = prepapre_dataframe(input_data, target_field, fields_to_keep)
-    # the idea is stolen from pandas.qcut
+
+    if validation:
+        assert validation_points is not None
+        assert validation_points != 0
+        assert validation_points >= bins
+        assert validation_points + output_samples <= gdf_out.shape[0]
+
+        # the idea is stolen from pandas.qcut
     # pd.qcut does not work for cases when it result in non-unique bin edges
     target = gdf_out[target_field].values
     if interval == 'percentile':
@@ -114,26 +120,26 @@ def resample_by_magnitude(input_data, target_field, bins=10, interval='linear', 
 
     dfs_to_concat = []
     validation_dfs_to_concat = []
-    total_samples = output_samples if output_samples else gdf_out.shape[0]
-    samples_per_bin = total_samples // bins
-
-    validate_array = np.ones(bins, dtype=np.bool)
-    if validation and bins > validation_points:
-        validate_array[validation_points:] = False
-        np.random.shuffle(validate_array)
-
+    approximate_samples_per_bin = gdf_out.shape[0] // bins
+    output_samples_per_bin = output_samples // bins
+    validation_samples_per_bin = validation_points // bins
+    assert approximate_samples_per_bin >= validation_samples_per_bin + output_samples_per_bin
     gb = gdf_out.groupby(BIN)
     for i, (b, gr) in enumerate(gb):
         if bootstrap:
             if undersample:
-                dfs_to_concat.append(gr.sample(n=samples_per_bin, replace=bootstrap))
+                dfs_to_concat.append(gr.sample(n=approximate_samples_per_bin, replace=bootstrap))
             else:
-                if gr.shape[0] < samples_per_bin:
-                    dfs_to_concat.append(gr.sample(n=samples_per_bin, replace=bootstrap))
+                if gr.shape[0] < approximate_samples_per_bin:
+                    dfs_to_concat.append(gr.sample(n=approximate_samples_per_bin, replace=bootstrap))
                 else:
                     dfs_to_concat.append(gr)
         else:
-            _df, v_df = _sample_without_replacement(gr, samples_per_bin, validate_array[i])
+            _df, v_df = _sample_without_replacement(
+                gr, output_samples_per_bin,
+                gr.shape[0] - output_samples_per_bin
+            )
+            assert gr.shape[0] - output_samples_per_bin + output_samples_per_bin <= gr.shape[0]
             dfs_to_concat.append(_df)
             validation_dfs_to_concat.append(v_df)
 
@@ -237,13 +243,13 @@ def create_grouping_polygons_from_geo_df(rows, cols, gdf_out):
     return polygons
 
 
-def _sample_without_replacement(df, samples_per_group, validate):
+def _sample_without_replacement(df, training_points, validation_points):
     """
     Parameters
     ----------
     df
-    samples_per_group
-    validate: bool
+    training_points
+    validation_points: bool
         whether to create a validate df
         if False, second dataframe returned will be empty
     Returns
@@ -251,11 +257,11 @@ def _sample_without_replacement(df, samples_per_group, validate):
 
     """
 
-    if df.shape[0] >= samples_per_group + 1:
+    if df.shape[0] >= training_points + validation_points:
         # if enough points take the number of samples
         # the second df returned makes up the validation shapefile
-        _df = df.sample(n=samples_per_group + 1, replace=False)
-        return _df.tail(samples_per_group), _df.head(int(validate))
+        # _df = df.sample(n=samples_per_group + 1, replace=False)
+        return df[:training_points], df[training_points:]
     else:
         # else take everything, this will lead to uncertain number of
         # points in the resulting shapefile
